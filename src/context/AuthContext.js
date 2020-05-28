dd40a4340423d825bd7d0de75f7a4a269409dc30
Sophie Moustard <sophie.moustard@gmail.com>
2020-05-28 09:37:14 +0200

    COM-1213 - persistant user session with Context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,34 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
+import Users from '../api/users';
+import { navigate } from '../navigationRef';
 
-const authreducer = (state, actions) => {
+const authReducer = (state, actions) => {
   switch (actions.type) {
+    case 'signin':
+      return { ...state, token: actions.payload };
     default:
       return state;
   }
 };
 
+const signIn = dispatch => async ({ email, password }) => {
+  const authentication = await Users.authenticate({ email, password });
+  await AsyncStorage.setItem('token', authentication.token);
+  dispatch({ type: 'signin', payload: authentication.token });
+  navigate('CourseList');
+};
+
+const tryLocalSignIn = dispatch => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    dispatch({ type: 'signin', payload: token });
+    navigate('CourseList');
+  } else navigate('Authentication');
+}
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signIn, tryLocalSignIn },
   { isSignedIn: false }
 );
--- src/screens/AuthenticationScreen.jsx
+++ src/screens/AuthenticationScreen.jsx
@@ -1,28 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, AsyncStorage } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import NiInput from '../components/form/Input';
 import NiButton from '../components/form/Button';
-import Users from '../api/users';
 
 const AuthenticationScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPasssword] = useState('');
-  const logIn = async () => {
-    try {
-      if (!email || !password) alert('Informations manquantes');
-      const authentication = await Users.authenticate({ email, password });
-      await AsyncStorage.setItem('token', authentication.token);
-      navigation.navigate('CourseList')
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
   return (
     <View style={styles.container}>
       <NiInput style={styles.input} caption="Email" value={email} onChangeText={setEmail} />
       <NiInput style={styles.input} caption="Mot de passe" value={password} onChangeText={setPasssword} />
-      <NiButton style={styles.button} caption="Connexion" onPress={logIn} />
+      <NiButton style={styles.button} caption="Connexion" onPress={() => signIn({ email, password })} />
     </View>
   );
 }
