aa58c52507e921ec71863fef29e0e82b8cb7f2b5
Sophie Moustard <sophie.moustard@gmail.com>
2020-05-28 09:37:14 +0200

    COM-1213 - persistant user session with Context

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {
-  createAppContainer,
-  createSwitchNavigator,
-} from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AuthenticationScreen from './src/screens/AuthenticationScreen';
 import ProgramListScreen from './src/screens/ProgramListScreen';
 import CourseListScreen from './src/screens/CourseListScreen';
 import CourseProfileScreen from './src/screens/CourseProfileScreen';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 import { Provider as AuthProvider } from './src/context/AuthContext';
+import { setNavigator } from './src/navigationRef';
 
 const switchNavigator = createSwitchNavigator({
+  ResolveAuthScreen,
   authenticationFlow: createStackNavigator({
     Authentication: AuthenticationScreen,
   }),
@@ -29,7 +29,7 @@ const App = createAppContainer(switchNavigator);
 export default () => {
   return (
     <AuthProvider>
-      <App />
+      <App ref={(navigator) => { setNavigator(navigator) }} />
     </AuthProvider>
   );
 }
--- src/api/users.js
+++ src/api/users.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const baseURL = 'http://36559f22.ngrok.io';
+const baseURL = 'http://e30e1879a750.ngrok.io';
 
 export default {
   authenticate: async (payload) => {
     const response = await axios.post(`${baseURL}/users/authenticate`, payload);
-    
-    console.log(response.data.data);  
     return response.data.data
   },
 };
