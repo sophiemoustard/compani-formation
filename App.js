e3c8d2de640086831ede8cc8f8ad6b63aa99350a
Sophie Moustard <sophie.moustard@gmail.com>
2020-06-10 12:13:05 +0200

    COM-1242 - fix statuss bar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar, View, StyleSheet } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AuthenticationScreen from './src/screens/Authentication';
@@ -7,6 +8,7 @@ import CourseListScreen from './src/screens/CourseList';
 import ResolveAuthScreen from './src/screens/ResolveAuth';
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { setNavigator } from './src/navigationRef';
+import variables from './src/styles/variables';
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuthScreen,
@@ -22,7 +24,22 @@ const App = createAppContainer(switchNavigator);
 export default () => {
   return (
     <AuthProvider>
+      <View style={[styles.statusBar]}>
+        <StatusBar translucent barStyle="dark-content" backgroundColor={variables.NEUTRAL_BACKGROUND_COLOR} />
+      </View>
       <App ref={(navigator) => { setNavigator(navigator) }} />
     </AuthProvider>
   );
 }
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    backgroundColor: variables.NEUTRAL_BACKGROUND_COLOR,
+    height: STATUSBAR_HEIGHT,
+  },
+});
