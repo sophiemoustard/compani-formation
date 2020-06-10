73e9fc513cf93c7398f77203a1b9015e053ca7b1
Sophie Moustard <sophie.moustard@gmail.com>
2020-06-10 11:42:33 +0200

    COM-1242 - remove authentication header

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AuthenticationScreen from './src/screens/Authentication';
 import ProgramListScreen from './src/screens/ProgramList';
@@ -11,9 +10,7 @@ import { setNavigator } from './src/navigationRef';
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuthScreen,
-  authenticationFlow: createStackNavigator({
-    Authentication: AuthenticationScreen,
-  }),
+  Authentication: AuthenticationScreen,
   mainFlow: createBottomTabNavigator({
     CourseList: CourseListScreen,
     ProgramList: ProgramListScreen,
