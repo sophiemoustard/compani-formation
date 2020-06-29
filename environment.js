837173506a2cbe32594c95eb3eed3332f3ec7bbc
Sophie Moustard <sophie.moustard@gmail.com>
2020-06-29 15:47:46 +0200

    Update env files

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
-import localEnv from './env/.env.local.js';
-import stagingEnv from './env/.env.staging.js';
+import localEnv from './env/env.local.js';
+import stagingEnv from './env/env.staging.js';
 
 const getEnvVars = (env = Constants.manifest.releaseChannel) => {
   if (__DEV__) return localEnv;
