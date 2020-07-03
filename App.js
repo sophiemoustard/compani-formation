a839d9437670cd247f6c1517bab4b5c7c17129e3
Sophie Moustard <sophie.moustard@gmail.com>
2020-07-03 11:58:55 +0200

    COM-1243 - refacto flow

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { StatusBar, View, StyleSheet, AppState, Linking } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AuthenticationScreen from './src/screens/Authentication';
 import ProgramListScreen from './src/screens/ProgramList';
 import CourseListScreen from './src/screens/CourseList';
-import ResolveAuthScreen from './src/screens/ResolveAuth';
-import { Provider as AuthProvider } from './src/context/AuthContext';
-import { setNavigator } from './src/navigationRef';
+import { Provider as AuthProvider, Context as AuthContext } from './src/context/AuthContext';
+import { navigationRef } from './src/navigationRef';
 import variables from './src/styles/variables';
 import getEnvVars from './environment';
 import Version from './src/api/version';
 import NiModal from './src/components/Modal';
 
-const switchNavigator = createSwitchNavigator({
-  ResolveAuthScreen,
-  Authentication: AuthenticationScreen,
-  mainFlow: createBottomTabNavigator({
-    CourseList: CourseListScreen,
-    ProgramList: ProgramListScreen,
-  })
-});
+const Tab = createBottomTabNavigator();
 
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ProgramList" component={ProgramListScreen} options={{ tabBarLabel: 'Catalogue!' }} />
+      <Tab.Screen name="CourseList" component={CourseListScreen} options={{ tabBarLabel: 'Mes formations' }} />
+    </Tab.Navigator>
+  );
+}
 
-const AppContainer = createAppContainer(switchNavigator);
+const Stack = createStackNavigator();
+
+function AppContainer () {
+  const { tryLocalSignIn, token } = useContext(AuthContext);
+  useEffect(() => { tryLocalSignIn(); }, []);
+
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator>
+        {token === null
+        ? <Stack.Screen name="Authentication" component={AuthenticationScreen} />
+        : <Stack.Screen name="Home" component={Home} />}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const App = () => {
   const appUrl = Platform.OS == 'ios'
     ? 'https://apps.apple.com/app/id1447513534'
@@ -62,7 +79,7 @@ const App = () => {
         <View style={[styles.statusBar]}>
           <StatusBar translucent barStyle="dark-content" backgroundColor={variables.NEUTRAL_BACKGROUND_COLOR} />
         </View>
-        <AppContainer ref={(navigator) => { setNavigator(navigator); }} />
+        <AppContainer />
       </AuthProvider>
     </>
   );
--- src/screens/CourseList.jsx
+++ src/screens/CourseList.jsx
@@ -1,24 +1,18 @@
 
-import React from 'react';
-import { Text, View, AsyncStorage } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, View } from 'react-native';
 import NiButton from '../components/form/Button';
 import screensStyle from '../styles/screens.style';
 import PropTypes from 'prop-types';
+import { Context as AuthContext } from '../context/AuthContext';
 
-const CourseListScreen = ({ navigation }) => {
-  const logOut = async () => {
-    try {
-      await AsyncStorage.removeItem('token');
-      navigation.navigate('Authentication');
-    } catch (e) {
-      console.log(e);
-    }
-  };
+const CourseListScreen = () => {
+  const { signOut } = useContext(AuthContext);
 
   return (
     <View style={screensStyle.container}>
       <Text>List of courses</Text>
-      <NiButton caption="Déconnexion" onPress={logOut} />
+      <NiButton caption="Déconnexion" onPress={signOut} />
     </View>
   );
 };
