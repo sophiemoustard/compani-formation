bcadd6228d6d1ec05e1f3e9b49fd43fa07b9fac7
Sophie Moustard <sophie.moustard@gmail.com>
2020-07-03 16:05:55 +0200

    COM-1243 - add logos on tab bar

--- src/screens/CourseList.jsx
+++ src/screens/CourseList.jsx
@@ -1,18 +1,12 @@
-
-import React, { useContext } from 'react';
+import React from 'react';
 import { Text, View } from 'react-native';
-import NiButton from '../components/form/Button';
 import screensStyle from '../styles/screens.style';
 import PropTypes from 'prop-types';
-import { Context as AuthContext } from '../context/AuthContext';
 
 const CourseListScreen = () => {
-  const { signOut } = useContext(AuthContext);
-
   return (
     <View style={screensStyle.container}>
-      <Text>List of courses</Text>
-      <NiButton caption="Déconnexion" onPress={signOut} />
+      <Text>Mes formations</Text>
     </View>
   );
 };
