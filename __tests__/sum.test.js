b503fecd3d431fcea4624514dc945c80593aab73
Chloe Molle <chloe.molle@neuf.fr>
2020-07-08 12:13:43 +0200

    COM-1301 install libraries to test app

---
+++ __tests__/sum.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, waitForElement, fireEvent } from 'react-native-testing-library';
+import { AuthenticationScreen } from '../src/screens/Authentication';
+
+test('should be truthy', () => {
+  expect(1).toBe(1);
+});
