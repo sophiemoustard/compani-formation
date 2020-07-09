a6d5e66fb6674b87bf13b5df82566b5c133385e4
Sophie Moustard <sophie.moustard@gmail.com>
2020-07-09 15:34:48 +0200

    COM-1350 - Add yellow spot - 1st draft

--- src/screens/CourseList.jsx
+++ src/screens/CourseList.jsx
@@ -3,6 +3,7 @@ import { Text, View, FlatList, StyleSheet } from 'react-native';
 import screensStyle from '../styles/screens.style';
 import { MAIN_MARGIN_LEFT } from '../styles/variables.js';
 import NextEvent from '../components/NextEvent';
+import Blob from '../components/Blob';
 
 const nextEvents = [
   { date: '2020-12-09T09:00:00', program: 'Identifier ses emotions pour mieux communiquer.', _id: '1' },
@@ -14,6 +15,9 @@ const CourseListScreen = () => {
   return (
     <View style={screensStyle.container}>
       <Text style={screensStyle.title}>Mes formations</Text>
+      <View style={styles.blobContainer}>
+        <Blob style={styles.blob} color="#FFEA95" />
+      </View>
       <Text style={screensStyle.subtitle}>Prochains évènements</Text>
       <View>
         <FlatList
@@ -32,7 +36,9 @@ const CourseListScreen = () => {
 const styles = StyleSheet.create({
   nextEventContainer: {
     paddingLeft: MAIN_MARGIN_LEFT,
-  }
+  },
+  blobContainer: { position: 'relative' },
+  blob: { position: 'absolute', top: -10 }
 });
 
 export default CourseListScreen;
