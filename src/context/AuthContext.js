67d7e5306577e6e91896e87b71c3e9633ddd8a9d
Chloe Molle <chloe.molle@neuf.fr>
2020-07-10 11:37:10 +0200

    COM-1301 remove async storage warning in tests

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import createDataContext from './createDataContext';
 import Users from '../api/users';
 import { navigate } from '../navigationRef';
