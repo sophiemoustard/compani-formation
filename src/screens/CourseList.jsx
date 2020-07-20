24c6c12e4a5a727d331756ab737af0b827f7ef39
Chloe Molle <chloe.molle@neuf.fr>
2020-07-20 14:56:03 +0200

    COM-1348 add begining of alenviAxios + first draft for displaying number of formation

--- src/screens/CourseList.jsx
+++ src/screens/CourseList.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { Text, View, FlatList, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import screensStyle from '../styles/screens.style';
 import { MAIN_MARGIN_LEFT } from '../styles/variables.js';
-import NextEvent from '../components/NextEvent';
+import Courses from '../api/courses';
 import Blob from '../components/Blob';
 
-const nextEvents = [
-  { date: '2020-12-09T09:00:00', program: 'Identifier ses emotions pour mieux communiquer.', _id: '1' },
-  { date: '2020-12-09T09:00:00', program: 'Identifier ses emotions pour mieux communiquer.', _id: '2' },
-  { date: '2020-12-09T09:00:00', program: 'Identifier ses emotions pour mieux communiquer.', _id: '3' }
-];
+class CourseListScreen extends Component {
+  constructor(props) {
+    super(props);
 
-const CourseListScreen = () => {
-  return (
-    <View style={screensStyle.container}>
-      <Text style={screensStyle.title} testID='header'>Mes formations</Text>
-      <View style={styles.blobContainer}>
-        <Blob style={styles.blob} color="#FFEA95" />
+    this.state = { courses: [] };
+  }
+
+  async componentDidMount () {
+    try {
+      const courses = await Courses.getByUser();
+      this.setState({ courses });
+    } catch (e) {
+      console.error(e);
+      this.setState({ courses: [] });
+    }
+  }
+
+  render () {
+    return (
+      <View style={screensStyle.container}>
+        <Text style={screensStyle.title} testID='header'>Mes formations</Text>
+        <View style={styles.blobContainer}>
+          <Blob style={styles.blob} color="#FFEA95" />
+        </View>
+        <Text style={screensStyle.subtitle}>Formations en cours ({this.state.courses.length})</Text>
       </View>
-      <Text style={screensStyle.subtitle}>Prochains évènements</Text>
-      <View>
-        <FlatList
-          horizontal
-          data={nextEvents}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => <NextEvent event={item} />}
-          style={{...styles.nextEventContainer}}
-          showsHorizontalScrollIndicator={false}
-        />
-      </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   nextEventContainer: {
