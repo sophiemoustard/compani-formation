f86679729384ad5c43915373f44aa8c1f5523864
Chloe Molle <chloe.molle@neuf.fr>
2020-07-22 10:33:02 +0200

    COM-1348 rename route

--- src/api/courses.js
+++ src/api/courses.js
@@ -2,9 +2,9 @@ import { alenviAxios } from './ressources/alenviAxios';
 import getEnvVars from '../../environment';
 
 export default {
-  getMyCourses: async (params) => {
+  getUserCourses: async (params) => {
     const { baseURL } = getEnvVars();
-    const response = await alenviAxios.get(`${baseURL}/courses/mine`, { params });
+    const response = await alenviAxios.get(`${baseURL}/courses/user`, { params });
     return response.data.data.courses;
   },
 };
