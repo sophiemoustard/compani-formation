fcafb27c7bfcb7afcde6b5554284b61c3bbdc04b
Sophie Moustard <sophie.moustard@gmail.com>
2020-07-31 10:53:17 +0200

    COM-1379 - add click

--- src/components/CourseCell.jsx
+++ src/components/CourseCell.jsx
@@ -3,22 +3,25 @@ import get from 'lodash/get';
 import PropTypes from 'prop-types';
 import { Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { GREY, WHITE } from '../styles/variables';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const CourseCell = ({ course }) => {
+const CourseCell = ({ course, navigation }) => {
   const programImage = get(course, 'program.image.link') || '';
   const source = programImage ? { uri: programImage } : require('../../assets/authentication_background_image.jpg');
   const programName = course.program.name.length > 50 ? `${course.program.name.slice(0, 48)}...` : course.program.name;
+  const goToCourse = () => navigation.navigate('Home', { screen: 'Courses', params: { screen: 'CourseProfile' } });
 
-  return <View style={styles.container}>
+  return <TouchableOpacity style={styles.container} onPress={goToCourse}>
     <View style={styles.imageContainer}>
       <ImageBackground source={source} imageStyle={styles.image} style={{ resizeMode: 'contain' }} />
     </View>
     <View style={styles.title}><Text>{programName}</Text></View>
-  </View>;
+  </TouchableOpacity>;
 };
 
 CourseCell.propTypes = {
   course: PropTypes.object,
+  navigation: PropTypes.object,
 };
 
 const imageHeight = 100;
