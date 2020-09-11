a1d0838d8f40683ab2c1105687c42f6fa0bb192a
KennyCallegari <kenny@alenvi.io>
2020-09-11 17:39:22 +0200

    COM-1464 - Fix review

--- src/api/activityHistories.ts
+++ src/api/activityHistories.ts
@@ -4,7 +4,6 @@ import getEnvVars from '../../environment';
 export default {
   postActivityHistories: async (payload) => {
     const { baseURL } = getEnvVars();
-    const response = await alenviAxios.post(`${baseURL}/activity-histories`, payload);
-    return response.data.message;
+    await alenviAxios.post(`${baseURL}/activityHistories`, payload);
   },
 };
