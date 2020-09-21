d817c3ac817810726e783b608353badf74c54ca0
Chloe Molle <chloe.molle@neuf.fr>
2020-09-21 12:30:33 +0200

    COM-1510 add actions/reducers in redux to add questionnaire answer

--- src/store/activities/actions.ts
+++ src/store/activities/actions.ts
@@ -2,11 +2,14 @@ import { ResetType } from '../../types/store/StoreType';
 import {
   SET_ACTIVITY,
   SET_CARD_INDEX,
+  ADD_QUESTIONNAIRE_ANSWER,
   SetActivityType,
   SetCardIndexType,
   SetExitConfirmationModalType,
   SET_EXIT_CONFIRMATION_MODAL,
   RESET_ACTIVITY_REDUCER,
+  QuestionnaireAnswer,
+  AddQuestionnaireAnswerType,
 } from '../../types/store/ActivityStoreType';
 import { ActivityType } from '../../types/ActivityType';
 
@@ -14,11 +17,14 @@ const setActivity = (activity: ActivityType): SetActivityType => ({ type: SET_AC
 const setCardIndex = (index: number): SetCardIndexType => ({ type: SET_CARD_INDEX, payload: index });
 const setExitConfirmationModal = (exitConfirmationModal: boolean): SetExitConfirmationModalType =>
   ({ type: SET_EXIT_CONFIRMATION_MODAL, payload: exitConfirmationModal });
+const addQuestionnaireAnswer = (questionnaireAnswer: QuestionnaireAnswer): AddQuestionnaireAnswerType =>
+  ({ type: ADD_QUESTIONNAIRE_ANSWER, payload: questionnaireAnswer });
 const resetActivityReducer = (): ResetType => ({ type: RESET_ACTIVITY_REDUCER });
 
 export default {
   setActivity,
   setCardIndex,
   setExitConfirmationModal,
+  addQuestionnaireAnswer,
   resetActivityReducer,
 };
--- src/store/activities/reducers.ts
+++ src/store/activities/reducers.ts
@@ -5,9 +5,27 @@ import {
   SET_CARD_INDEX,
   SET_EXIT_CONFIRMATION_MODAL,
   RESET_ACTIVITY_REDUCER,
+  ADD_QUESTIONNAIRE_ANSWER,
 } from '../../types/store/ActivityStoreType';
 
-const initialState: ActivityStateType = { activity: null, cardIndex: null, exitConfirmationModal: false };
+const initialState: ActivityStateType = {
+  activity: null,
+  cardIndex: null,
+  exitConfirmationModal: false,
+  allQuestionnaireAnswers: [],
+};
+
+const applyAddQuestionnaireAnswer = (state, action) => {
+  const questionnaireAnswer = action.payload;
+  const indexOfQuestionnaireAnswer = state.allQuestionnaireAnswers.findIndex((qa => qa.id === questionnaireAnswer.id));
+  if (indexOfQuestionnaireAnswer > -1) {
+    const newAllQuestionnaireAnswers = [...state.allQuestionnaireAnswers];
+    newAllQuestionnaireAnswers[indexOfQuestionnaireAnswer] = questionnaireAnswer;
+    return { ...state, allQuestionnaireAnswers: newAllQuestionnaireAnswers };
+  }
+
+  return { ...state, allQuestionnaireAnswers: [...state.allQuestionnaireAnswers, questionnaireAnswer] };
+};
 
 export const activities = (
   state: ActivityStateType = initialState,
@@ -20,6 +38,8 @@ export const activities = (
       return { ...state, cardIndex: action.payload };
     case SET_EXIT_CONFIRMATION_MODAL:
       return { ...state, exitConfirmationModal: action.payload };
+    case ADD_QUESTIONNAIRE_ANSWER:
+      return applyAddQuestionnaireAnswer(state, action);
     case RESET_ACTIVITY_REDUCER:
       return initialState;
     default:
