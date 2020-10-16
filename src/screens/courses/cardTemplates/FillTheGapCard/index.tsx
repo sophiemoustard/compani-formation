e0c57fadd242879f88ec779d0e058dfabb8e3914
ulysse <ulysse@alenvi.io>
2020-10-16 16:17:46 +0200

    COM-1519 : in progress

--- src/screens/courses/cardTemplates/FillTheGapCard/index.tsx
+++ src/screens/courses/cardTemplates/FillTheGapCard/index.tsx
@@ -1,64 +1,164 @@
-import React from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Text, ScrollView, View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import shuffle from 'lodash/shuffle';
+import { DraxProvider, DraxView } from 'react-native-drax';
 import { StateType } from '../../../../types/store/StoreType';
 import Selectors from '../../../../store/activities/selectors';
 import { FillTheGapType } from '../../../../types/CardType';
 import CardHeader from '../../../../components/cards/CardHeader';
-import { FILL_THE_GAPS } from '../../../../core/data/constants';
 import cardsStyle from '../../../../styles/cards';
 import styles from './styles';
 import QuestionCardFooter from '../../../../components/cards/QuestionCardFooter';
-import { PINK } from '../../../../styles/colors';
+import { PINK, GREY, GREEN, ORANGE } from '../../../../styles/colors';
+import Shadow from '../../../../components/design/Shadow';
+import { navigate } from '../../../../navigationRef';
+import Actions from '../../../../store/activities/actions';
 
 interface FillTheGap {
   card: FillTheGapType,
-  index: number
+  index: number,
+  isFocused: boolean,
+  incGoodAnswersCount: () => void,
 }
 
-const FillTheGapCard = ({ card, index }: FillTheGap) => {
-  if (!card || card.template !== FILL_THE_GAPS) return null;
+interface footerColorsType {
+  buttonsColor: string,
+  textColor: string,
+  backgroundColor: string,
+}
 
-  const goodAnswers = card.gappedText?.match(/<trou>[^<]*<\/trou>/g)?.map(rep => rep.replace(/<\/?trou>/g, '')) || [];
-  const answers = shuffle([...card.falsyGapAnswers, ...goodAnswers]);
+const FillTheGapCard = ({ card, index, isFocused, incGoodAnswersCount }: FillTheGap) => {
+  const goodAnswers = useRef<Array<string>>([]);
+  const [propositions, setPropositions] = useState<Array<any>>([]);
+  const [received, setReceived] = useState<Array<any>>([]);
+  const [isValidated, setIsValidated] = useState<boolean>(false);
+  const [isAnsweredCorrectly, setIsAnsweredCorrectly] = useState<boolean>(false);
+  const [footerColors, setFooterColors] = useState<footerColorsType>({
+    buttonsColor: PINK[500],
+    textColor: GREY[100],
+    backgroundColor: GREY[100],
+  });
 
-  const renderAnswers = () => <View style={styles.answersContainer}>
-    {answers.map((txt, idx) => <Text style={styles.answer} key={`answer${idx}`}>{txt}</Text>)}
+  useEffect(() => {
+    if (card && isFocused) {
+      goodAnswers.current = card.gappedText.match(/<trou>[^<]*<\/trou>/g)?.map(rep => rep.replace(/<\/?trou>/g, '')) ||
+    [];
+      setPropositions(shuffle([...card.falsyGapAnswers, ...goodAnswers.current]));
+    }
+  }, [card, isFocused]);
+
+  useEffect(() => {
+    if (!isValidated) {
+      return setFooterColors({ buttonsColor: PINK[500], textColor: GREY[100], backgroundColor: GREY[100] });
+    }
+
+    if (isAnsweredCorrectly) {
+      return setFooterColors({ buttonsColor: GREEN[600], textColor: GREEN[600], backgroundColor: GREEN[100] });
+    }
+
+    return setFooterColors({ buttonsColor: ORANGE[600], textColor: ORANGE[600], backgroundColor: ORANGE[100] });
+  }, [isValidated, isAnsweredCorrectly]);
+
+  if (!isFocused) return null;
+
+  const style = styles(footerColors.textColor, footerColors.backgroundColor);
+
+  const renderAnswers = () => <View style={style.answersContainer} pointerEvents={isValidated ? 'none' : 'auto'}>
+    {propositions.map((txt, idx) =>
+      <View style={style.empty} key={`answer${idx}`}>
+        <DraxView
+          style={style.answerContainer}
+          draggingStyle={{ opacity: 0 }}
+          // dragReleasedStyle={{ opacity: 0 }}
+          dragPayload={txt}
+          longPressDelay={0}>
+          <View style={txt ? style.textContainer : { opacity: 0 }}>
+            <Text style={style.answer}>{txt}</Text>
+          </View>
+          <Shadow customStyle={style.shadow} />
+        </DraxView>
+      </View>)}
   </View>;
 
   const renderQuestion = (text) => {
     const splittedText = text.split(/<trou>[^<]*<\/trou>/g);
-    return <View style={[cardsStyle.question, styles.questionContainer]}>
+    return <View style={[cardsStyle.question, style.questionContainer]}>
       {
         splittedText.map((txt, idx) => {
-          if (idx === 0 && txt === '') return <View style={styles.gapContainer} key={`gap${idx}`} />;
+          if (idx === 0 && txt === '') return <View style={style.gapContainer} key={`gap${idx}`} />;
           if (idx === splittedText.length - 1 && txt === '') return null;
           if (idx < splittedText.length - 1) {
             return <>
-              <Text style={styles.question} key={`text${idx}`}>{txt}</Text>
-              <View style={styles.gapContainer} key={`gap${idx}`} />
+              <Text style={style.question} key={`text${idx}`}>{txt}</Text>
+              {/* <View style={styles.gapContainer} key={`gap${idx}`} /> */}
+              <DraxView
+                style={!received[idx] ? style.gapContainer : style.answerContainer}
+                // receivingStyle={styles.receiving}
+                renderContent={() => received[idx] &&
+                  <>
+                    <View style={style.textContainer}>
+                      <Text style={style.answer}>{received[idx]}</Text>
+                    </View>
+                    <Shadow customStyle={style.shadow} />
+                  </>
+                }
+                onReceiveDragDrop={(event) => {
+                  const tempArray = [...received];
+                  const tempArray2 = [...propositions];
+                  if (tempArray[idx]) {
+                    tempArray2[tempArray2.indexOf(event.dragged.payload)] = tempArray[idx];
+                  } else {
+                    tempArray2[tempArray2.indexOf(event.dragged.payload)] = '';
+                  }
+                  tempArray[idx] = event.dragged.payload;
+                  setReceived(tempArray);
+                  setPropositions(tempArray2);
+                }}
+              />
             </>;
           }
-          return <Text style={styles.question} key={`text${idx}`}>{txt}</Text>;
+          return <Text style={style.question} key={`text${idx}`}>{txt}</Text>;
         })
       }
     </View>;
   };
 
+  const onPressFooterButton = () => {
+    if (!isValidated) {
+      const areAnswersCorrect = received.every((receive, idx) => (receive === goodAnswers.current[idx]));
+      setIsAnsweredCorrectly(areAnswersCorrect);
+      if (areAnswersCorrect) incGoodAnswersCount();
+
+      return setIsValidated(true);
+    }
+    return navigate(`card-${index + 1}`);
+  };
+
   return (
     <>
       <CardHeader />
-      <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
-        {/* {renderQuestion(card.gappedText)}
-        {renderAnswers()} */}
-        <Text>testesteteste</Text>
+      <ScrollView contentContainerStyle={style.container} showsVerticalScrollIndicator={false}>
+        <DraxProvider>
+        {console.log('afficher question', isFocused, card)}
+          {isFocused && card.gappedText && renderQuestion(card.gappedText)}
+          {renderAnswers()}
+        </DraxProvider>
       </ScrollView>
-      <QuestionCardFooter index={index} arrowColor={PINK[500]} buttonColor={PINK[500]} />
+      <View style={style.footerContainer}>
+        {isValidated && <Text style={[cardsStyle.explanation, style.explanation]}>{card.explanation}</Text>}
+        <QuestionCardFooter onPressButton={onPressFooterButton} buttonCaption={isValidated ? 'Continuer' : 'Valider'}
+          arrowColor={footerColors.buttonsColor} index={index}
+          buttonDisabled={!(received.length === goodAnswers.current.length)}
+          buttonColor={(received.length === goodAnswers.current.length) ? footerColors.buttonsColor : GREY[300]} />
+      </View>
     </>
   );
 };
 
 const mapStateToProps = (state: StateType) => ({ card: Selectors.getCard(state), index: state.activities.cardIndex });
+const mapDispatchToProps = dispatch => ({
+  incGoodAnswersCount: () => dispatch(Actions.incGoodAnswersCount()),
+});
 
-export default connect(mapStateToProps)(FillTheGapCard);
+export default connect(mapStateToProps, mapDispatchToProps)(FillTheGapCard);
--- src/screens/courses/cardTemplates/FillTheGapCard/styles.ts
+++ src/screens/courses/cardTemplates/FillTheGapCard/styles.ts
@@ -1,22 +1,21 @@
 import { StyleSheet } from 'react-native';
-import { GREY, TRANSPARENT_LIGHT_GREY } from '../../../../styles/colors';
-import { BORDER_RADIUS, INPUT_HEIGHT, MARGIN, GAP_WIDTH, BORDER_WIDTH, PADDING } from '../../../../styles/metrics';
+import { GREY, TRANSPARENT_LIGHT_GREY, WHITE } from '../../../../styles/colors';
+import { BORDER_RADIUS, INPUT_HEIGHT, MARGIN, GAP_WIDTH, BORDER_WIDTH, PADDING, ABSOLUTE_BOTTOM_POSITION } from '../../../../styles/metrics';
 import { FIRA_SANS_MEDIUM } from '../../../../styles/fonts';
 
-const styles = StyleSheet.create({
+const styles = (textColor: string, backgroundColor: string) => StyleSheet.create({
   container: {
-    alignItems: 'center',
     justifyContent: 'flex-start',
     flexGrow: 1,
   },
   questionContainer: {
     ...FIRA_SANS_MEDIUM.MD,
-    marginHorizontal: MARGIN.LG,
+    // marginHorizontal: MARGIN.LG,
     width: '100%',
     flexWrap: 'wrap',
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    alignItems: 'flex-end',
+    alignItems: 'center',
     paddingTop: PADDING.XXL,
     paddingBottom: PADDING.XL,
     paddingHorizontal: PADDING.LG,
@@ -33,24 +32,55 @@ const styles = StyleSheet.create({
     height: INPUT_HEIGHT,
     borderRadius: BORDER_RADIUS.MD,
     backgroundColor: GREY[200],
-    marginHorizontal: MARGIN.MD,
+    marginHorizontal: MARGIN.XS,
+    marginVertical: MARGIN.XS,
+  },
+  shadow: {
+    backgroundColor: GREY[200],
+    borderRadius: BORDER_RADIUS.MD,
   },
   answersContainer: {
     flexWrap: 'wrap',
     flexDirection: 'row',
     justifyContent: 'center',
   },
-  answer: {
-    ...FIRA_SANS_MEDIUM.MD,
+  answerContainer: {
+    // marginHorizontal: MARGIN.MD,
+    // marginBottom: MARGIN.MD,
     height: 40,
+  },
+  textContainer: {
+    backgroundColor: WHITE,
     borderWidth: BORDER_WIDTH,
     borderColor: GREY[200],
     borderRadius: BORDER_RADIUS.MD,
+    flex: 1,
+  },
+  answer: {
+    ...FIRA_SANS_MEDIUM.MD,
     padding: PADDING.MD,
     textAlign: 'center',
     color: GREY[800],
-    marginHorizontal: MARGIN.XS,
-    marginBottom: MARGIN.MD,
+  },
+  empty: {
+    backgroundColor: GREY[200],
+    borderRadius: BORDER_RADIUS.MD,
+    marginHorizontal: MARGIN.SM,
+    marginBottom: MARGIN.SM,
+    height: 40,
+    minWidth: 100,
+  },
+  explanation: {
+    color: textColor,
+    minHeight: INPUT_HEIGHT,
+    position: 'absolute',
+    right: 0,
+    left: 0,
+    bottom: ABSOLUTE_BOTTOM_POSITION,
+    backgroundColor,
+  },
+  footerContainer: {
+    backgroundColor,
   },
 });
 
