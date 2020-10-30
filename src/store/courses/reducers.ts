6cddb6c759a82e3256e92b93afd73aa4eec03520
ulysse <ulysse@alenvi.io>
2020-10-30 09:57:23 +0100

    COM-1598: change type

--- src/store/courses/reducers.ts
+++ src/store/courses/reducers.ts
@@ -1,14 +1,14 @@
 import {
   CourseStateType,
   SET_IS_COURSE,
-  CourseActionType,
+  CourseActionWithoutPayloadType,
 } from '../../types/store/CourseStoreType';
 
 const initialState: CourseStateType = { isCourse: true };
 
 export const courses = (
   state: CourseStateType = initialState,
-  action: CourseActionType
+  action: CourseActionWithoutPayloadType
 ): CourseStateType => {
   switch (action.type) {
     case SET_IS_COURSE:
