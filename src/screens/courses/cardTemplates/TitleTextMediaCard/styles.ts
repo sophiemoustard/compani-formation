9df737aa2bc33ce9534a3367b8543c1da6ec11ab
ulysse <ulysse@alenvi.io>
2020-11-03 15:19:53 +0100

    COM-1602: fix image margin

--- src/screens/courses/cardTemplates/TitleTextMediaCard/styles.ts
+++ src/screens/courses/cardTemplates/TitleTextMediaCard/styles.ts
@@ -1,16 +1,16 @@
 import { StyleSheet } from 'react-native';
-import { MARGIN, PADDING } from '../../../../styles/metrics';
+import { MARGIN } from '../../../../styles/metrics';
 import cardsStyle from '../../../../styles/cards';
 
-const styles = (imgHeight : number) => StyleSheet.create({
+const styles = (imgHeight: number) => StyleSheet.create({
   container: {
     flex: 1,
     marginHorizontal: MARGIN.MD,
-    paddingBottom: PADDING.XL,
   },
   image: {
     ...cardsStyle.media,
     height: imgHeight,
+    marginBottom: MARGIN.LG,
   },
 });
 
