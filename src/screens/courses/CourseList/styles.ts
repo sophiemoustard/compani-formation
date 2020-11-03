d82597efde6251a55a87f5309c94777f379dc20b
ulysse <ulysse@alenvi.io>
2020-11-03 17:17:23 +0100

    COM-1602-quickfix: change icon color

--- src/screens/courses/CourseList/styles.ts
+++ src/screens/courses/CourseList/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { MAIN_MARGIN_LEFT } from '../../../styles/metrics';
-import { PINK, YELLOW } from '../../../styles/colors';
+import { PINK, PURPLE, YELLOW } from '../../../styles/colors';
 import { FIRA_SANS_BOLD } from '../../../styles/fonts';
 
 const styles = StyleSheet.create({
@@ -13,9 +13,16 @@ const styles = StyleSheet.create({
   coursesCountContainer: {
     backgroundColor: YELLOW[200],
   },
+  subProgramsCountContainer: {
+    backgroundColor: PURPLE[200],
+  },
   coursesCount: {
     ...FIRA_SANS_BOLD.MD,
     color: YELLOW[800],
+  },
+  subProgramsCount: {
+    ...FIRA_SANS_BOLD.MD,
+    color: PURPLE[800],
   },
   nextEventsCountContainer: {
     backgroundColor: PINK[100],
