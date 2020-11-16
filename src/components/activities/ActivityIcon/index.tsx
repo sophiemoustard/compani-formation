674393d3902692bbd6b8125a1b4391052a1d3957
ulysse <ulysse@alenvi.io>
2020-11-16 11:31:35 +0100

    COM-1632: design for questionnaire type activity

--- src/components/activities/ActivityIcon/index.tsx
+++ src/components/activities/ActivityIcon/index.tsx
@@ -3,10 +3,11 @@ import { View } from 'react-native';
 import VideoIcon from '../../../../assets/icons/VideoIcon';
 import LessonIcon from '../../../../assets/icons/LessonIcon';
 import QuizIcon from '../../../../assets/icons/QuizIcon';
+import QuestionnaireIcon from '../../../../assets/icons/QuestionnaireIcon';
 import SharingExperienceIcon from '../../../../assets/icons/SharingExperienceIcon';
 import commonStyle from '../../../styles/common';
 import { ActivityType } from '../../../types/ActivityType';
-import { SHARING_EXPERIENCE, LESSON, QUIZ, VIDEO } from '../../../core/data/constants';
+import { SHARING_EXPERIENCE, LESSON, QUIZ, VIDEO, QUESTIONNAIRE } from '../../../core/data/constants';
 import Shadow from '../../design/Shadow';
 import styles from './styles';
 
@@ -26,10 +27,11 @@ const ActivityIcon = ({ activity, disabled, borderColor, backgroundColor }: Acti
   const coloredStyle = styles({ borderColor, backgroundColor });
 
   const getIcon = () => {
-    if (activity.type === SHARING_EXPERIENCE) return <SharingExperienceIcon style={coloredStyle.icon}/>;
-    if (activity.type === LESSON) return <LessonIcon style={coloredStyle.icon}/>;
-    if (activity.type === QUIZ) return <QuizIcon style={coloredStyle.icon}/>;
-    if (activity.type === VIDEO) return <VideoIcon style={coloredStyle.icon}/>;
+    if (activity.type === SHARING_EXPERIENCE) return <SharingExperienceIcon style={coloredStyle.icon} />;
+    if (activity.type === LESSON) return <LessonIcon style={coloredStyle.icon} />;
+    if (activity.type === QUIZ) return <QuizIcon style={coloredStyle.icon} />;
+    if (activity.type === VIDEO) return <VideoIcon style={coloredStyle.icon} />;
+    if (activity.type === QUESTIONNAIRE) return <QuestionnaireIcon style={coloredStyle.icon} />;
     return null;
   };
 
