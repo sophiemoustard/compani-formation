82b9c02965b1162035f5efb84e61f40affe65a20
Chloe Molle <chloe.molle@neuf.fr>
2020-11-18 08:55:27 +0100

    COM-1635 fix orientation on android + hide nativeControls until the video is ready to be played

--- src/components/cards/Video/index.tsx
+++ src/components/cards/Video/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { Platform } from 'react-native';
 import { Video } from 'expo-av';
 import styles from './styles';
@@ -11,13 +12,14 @@ interface NiVideoProps {
 }
 
 const NiVideo = ({ mediaSource }: NiVideoProps) => {
-  const [playVisible, setPlayVisible] = useState<Boolean>(true);
+  const [playVisible, setPlayVisible] = useState<boolean>(true);
+  const [nativeControlsVisible, setNativeControlsVisible] = useState<boolean>(false);
   const videoRef = useRef<Video>(null);
   const isIos = Platform.OS === 'ios';
   let iosVersion;
   if (isIos) iosVersion = Platform.Version;
 
-  const displayFullScreen = () => {
+  const displayFullscreen = () => {
     if (!isIos) videoRef.current?.presentFullscreenPlayer();
     videoRef.current?.playAsync();
   };
@@ -27,13 +29,31 @@ const NiVideo = ({ mediaSource }: NiVideoProps) => {
     else setPlayVisible(true);
   };
 
+  // eslint-disable-next-line consistent-return
+  const onFullscreenUpdate = async ({ fullscreenUpdate }) => {
+    if (Platform.OS === 'android') {
+      switch (fullscreenUpdate) {
+        case Video.FULLSCREEN_UPDATE_PLAYER_DID_PRESENT:
+          return ScreenOrientation.unlockAsync();
+        case Video.FULLSCREEN_UPDATE_PLAYER_WILL_DISMISS:
+          return ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+        default:
+      }
+    }
+  };
+
+  const onReadyForDisplay = ({ status }) => {
+    if (status.isLoaded) setNativeControlsVisible(true);
+  };
+
   return (
     <>
       {playVisible && (!isIos || (isIos && iosVersion === '14.1')) &&
-        <IconButton name='play-circle' size={ICON.XXL} onPress={displayFullScreen} color={GREY[100]}
+        <IconButton name='play-circle' size={ICON.XXL} onPress={displayFullscreen} color={GREY[100]}
           style={styles.play} />}
-      <Video ref={videoRef} useNativeControls resizeMode='contain' source={mediaSource} style={styles.media}
-        onPlaybackStatusUpdate={onPlaybackStatusUpdate} />
+      <Video ref={videoRef} useNativeControls={nativeControlsVisible} resizeMode='contain' source={mediaSource}
+        onPlaybackStatusUpdate={onPlaybackStatusUpdate} onFullscreenUpdate={onFullscreenUpdate} style={styles.media}
+        onReadyForDisplay={onReadyForDisplay} />
     </>
   );
 };
