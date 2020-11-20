8b33dc78ff598d57d22e751b1c546b506a6ef02a
Chloe Molle <chloe.molle@neuf.fr>
2020-11-20 10:59:43 +0100

    COM-1636 handle play and pause

--- src/components/cards/Audio/index.tsx
+++ src/components/cards/Audio/index.tsx
@@ -13,6 +13,7 @@ interface NiAudioProps {
 
 const NiAudio = ({ mediaSource }: NiAudioProps) => {
   const [soundObject] = useState(new Audio.Sound()); // state needed because of the useEffect
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   useEffect(() => {
     async function loadAudio() {
@@ -38,18 +39,30 @@ const NiAudio = ({ mediaSource }: NiAudioProps) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const playAudio = async () => {
+  const playOrPauseAudio = async () => {
     try {
       const status = await soundObject.getStatusAsync();
-      if (mediaSource && status.isLoaded) await soundObject.playFromPositionAsync(0);
+      if (mediaSource && status.isLoaded && !status.isPlaying) {
+        const { positionMillis, durationMillis } = status;
+        const position = !positionMillis || positionMillis === durationMillis ? 0 : positionMillis;
+        await soundObject.playFromPositionAsync(position);
+      }
+      if (mediaSource && status.isLoaded && status.isPlaying) await soundObject.pauseAsync();
     } catch (e) {
       console.error(e);
     }
   };
 
+  const onPlaybackStatusUpdate = async (playbackStatus) => {
+    setIsPlaying(playbackStatus?.isPlaying || false);
+  };
+
+  soundObject.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+
   return (
     <View style={styles.container}>
-      <IconButton name='ios-play' size={ICON.MD} onPress={playAudio} color={GREY[800]} iconFamily={IONICONS} />
+      <IconButton name={isPlaying ? 'ios-pause' : 'ios-play'} size={ICON.MD} onPress={playOrPauseAudio}
+        color={GREY[800]} iconFamily={IONICONS} />
     </View>
   );
 };
