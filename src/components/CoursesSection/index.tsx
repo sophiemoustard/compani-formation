3dcc547562a53390543e7e884b715b881f44a598
ulysse <ulysse@alenvi.io>
2020-12-11 19:01:19 +0100

    COM-1680: update design

--- src/components/CoursesSection/index.tsx
+++ src/components/CoursesSection/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { View, Text, FlatList, StyleProp, ViewStyle } from 'react-native';
+import { View, Text, FlatList, StyleProp, ViewStyle, ImageBackground, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import { SubProgramType } from '../../types/SubProgramType';
 import { CourseType } from '../../types/CourseType';
 import { ProgramType } from '../../types/ProgramType';
 import { formatWordToPlural } from '../../core/helpers/utils';
+import NiButton from '../../components/form/Button';
+import { PINK, WHITE } from '../../styles/colors';
+import { navigate } from '../../navigationRef';
 
 interface CoursesSectionProps {
   items: Array<ProgramType | CourseType | SubProgramType>,
   title: string,
   type?: string,
   countStyle: StyleProp<ViewStyle>
+  showCatalogButton?: boolean,
   renderItem: (item) => JSX.Element,
 }
 
@@ -19,6 +23,7 @@ const CoursesSection = ({
   title,
   type = 'FORMATION',
   countStyle,
+  showCatalogButton = false,
   renderItem,
 }: CoursesSectionProps) => {
   const renderSeparator = () => <View style={styles.separator} />;
@@ -32,6 +37,17 @@ const CoursesSection = ({
       <FlatList horizontal data={items} keyExtractor={item => item._id} style={styles.container}
         renderItem={({ item }) => renderItem(item)} showsHorizontalScrollIndicator={false}
         ItemSeparatorComponent={renderSeparator} />
+      {showCatalogButton &&
+      <TouchableOpacity style={styles.courseContainer}>
+        <ImageBackground imageStyle={styles.goToCatalogBackground} style={styles.goToCatalog}
+          source={require('../../../assets/images/go-to-catalog.png')}>
+          <Text style={styles.text}>Tu n’as pas de formation en cours...</Text>
+          <NiButton caption="Chercher une formation" onPress={() => navigate('Catalog')}
+            bgColor={PINK[500]}
+            color={WHITE} borderColor={PINK[500]} />
+        </ImageBackground>
+      </TouchableOpacity>
+      }
     </>
   );
 };
--- src/screens/courses/CourseList/index.tsx
+++ src/screens/courses/CourseList/index.tsx
@@ -19,8 +19,6 @@ import SubPrograms from '../../../api/subPrograms';
 import { TRAINING_ORGANISATION_MANAGER, VENDOR_ADMIN } from '../../../core/data/constants';
 import { ActionWithoutPayloadType } from '../../../types/store/StoreType';
 import CoursesSection from '../../../components/CoursesSection';
-import NiButton from '../../../components/form/Button';
-import { PINK, WHITE } from '../../../styles/colors';
 
 interface CourseListProps {
   setIsCourse: (value: boolean) => void,
@@ -107,8 +105,6 @@ const CourseList = ({ setIsCourse, navigation, loggedUserId, userVendorRole }: C
     else navigation.navigate('SubProgramProfile', { subProgramId: id });
   };
 
-  const goToCatalog = () => navigation.navigate('Catalog');
-
   const onPressProgramCell = (isCourse, id) => {
     setIsCourse(isCourse);
     goToCourse(id, isCourse);
@@ -131,22 +127,14 @@ const CourseList = ({ setIsCourse, navigation, loggedUserId, userVendorRole }: C
       {nextSteps.length > 0 &&
         <View style={styles.nextSteps}>
           <CoursesSection items={nextSteps} title='Mes prochains rendez-vous' countStyle={styles.nextEventsCount}
-            renderItem={renderNexStepsItem} type={'ÉVÉNEMENT'}/>
+            renderItem={renderNexStepsItem} type={'ÉVÉNEMENT'} />
         </View>
       }
-      {onGoingCourses.length > 0
-        ? <ImageBackground imageStyle={styles.onGoingAndDraftBackground} style={styles.sectionContainer}
-          source={require('../../../../assets/images/ongoing_background.png')}>
-          <CoursesSection items={onGoingCourses} title='Mes formations en cours' renderItem={renderCourseItem}
-            countStyle={styles.onGoingCoursesCount} />
-        </ImageBackground>
-        : <ImageBackground imageStyle={styles.goToCatalogBackground} style={styles.goToCatalog}
-          source={require('../../../../assets/images/go-to-catalog.png')}>
-          <Text style={styles.text}>Tu n’as pas de formation en cours...</Text>
-          <NiButton caption="Chercher une formation" onPress={goToCatalog} bgColor={PINK[500]} color={WHITE}
-            borderColor={PINK[500]} />
-        </ImageBackground>
-      }
+      <ImageBackground imageStyle={styles.onGoingAndDraftBackground} style={styles.sectionContainer}
+        source={require('../../../../assets/images/ongoing_background.png')}>
+        <CoursesSection items={onGoingCourses} title='Mes formations en cours' renderItem={renderCourseItem}
+          countStyle={styles.onGoingCoursesCount} showCatalogButton={onGoingCourses.length === 0}/>
+      </ImageBackground>
       {achievedCourses.length > 0 &&
         <ImageBackground imageStyle={styles.achievedBackground} style={styles.sectionContainer}
           source={require('../../../../assets/images/achieved_background.png')}>
