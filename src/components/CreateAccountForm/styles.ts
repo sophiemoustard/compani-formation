65b47e85c8a8f7a9d662b11e3e9fa565cfd8d10c
ulysse <ulysse@alenvi.io>
2020-12-11 10:32:07 +0100

    COM-1653: rename variable

--- src/components/CreateAccountForm/styles.ts
+++ src/components/CreateAccountForm/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 import { MARGIN } from '../../styles/metrics';
 import { FIRA_SANS_BOLD } from '../../styles/fonts';
 
-const styles = isKeyboardOpen => StyleSheet.create({
+const styles = isTooClose => StyleSheet.create({
   title: {
     ...FIRA_SANS_BOLD.LG,
     marginVertical: MARGIN.LG,
@@ -19,7 +19,7 @@ const styles = isKeyboardOpen => StyleSheet.create({
   },
   footer: {
     marginTop: MARGIN.LG,
-    marginBottom: isKeyboardOpen ? MARGIN.MD : (MARGIN.XL + MARGIN.MD),
+    marginBottom: isTooClose ? MARGIN.MD : (MARGIN.XL + MARGIN.MD),
     justifyContent: 'flex-end',
     flex: 1,
   },
