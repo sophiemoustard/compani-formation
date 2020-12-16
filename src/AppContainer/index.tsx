fe3c3a4114f6ded9ae457b340909e24cbf79cf5e
Chloe Molle <chloe.molle@neuf.fr>
2020-12-16 15:25:11 +0100

    COM-1701 rename first connection and use it instead of forgotPassword

--- src/AppContainer/index.tsx
+++ src/AppContainer/index.tsx
@@ -13,8 +13,7 @@ import Camera from '../screens/Camera';
 import { Context as AuthContext } from '../context/AuthContext';
 import { navigationRef } from '../navigationRef';
 import Authentication from '../screens/Authentication';
-import ForgotPassword from '../screens/ForgotPassword';
-import FirstConnection from '../screens/FirstConnection';
+import EmailForm from '../screens/EmailForm';
 import CreateAccount from '../screens/CreateAccount';
 import Catalog from '../screens/explore/Catalog';
 import About from '../screens/explore/About';
@@ -93,7 +92,7 @@ const AppContainer = ({ setLoggedUser, statusBarVisible }: AppContainerProps) =>
 
   const style = styles(statusBarVisible, StatusBar.currentHeight || 20);
 
-  const authScreens = { Authentication, ForgotPassword, FirstConnection, CreateAccount };
+  const authScreens = { Authentication, EmailForm, CreateAccount };
 
   const Profile = { ProfileEdition, PasswordEdition, Camera };
   const Courses = { CourseProfile, SubProgramProfile };
