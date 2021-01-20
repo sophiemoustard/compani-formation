3da60ae614915db6c432c80aa631fd45abb3e513
ulysse <ulysse@alenvi.io>
2021-01-20 19:08:55 +0100

    COM-1806: access to about from course

--- src/screens/explore/About/index.tsx
+++ src/screens/explore/About/index.tsx
@@ -1,45 +1,30 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Image, Text, View, ScrollView, BackHandler } from 'react-native';
 import { connect } from 'react-redux';
 import Markdown from 'react-native-markdown-display';
-import { CommonActions, StackActions, StackActionType } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import get from 'lodash/get';
-import { Context as AuthContext } from '../../../context/AuthContext';
 import styles from './styles';
 import { markdownStyle } from '../../../styles/common';
 import { WHITE } from '../../../styles/colors';
 import { ICON } from '../../../styles/metrics';
 import Button from '../../../components/form/Button';
 import FeatherButton from '../../../components/icons/FeatherButton';
-import Courses from '../../../api/courses';
-import { getLoggedUserId } from '../../../store/main/selectors';
 import CoursesActions from '../../../store/courses/actions';
 import { ActionWithoutPayloadType } from '../../../types/store/StoreType';
+import { ProgramType } from '../../../types/ProgramType';
 
 interface AboutProps {
-  route: { params: { program } },
-  navigation: {
-    navigate: (path: string, params?: object) => {},
-    dispatch: (action: CommonActions.Action | StackActionType) => {}},
-  loggedUserId: string,
-  setIsCourse: (value: boolean) => void,
+  program: ProgramType,
+  buttonCaption?: string,
+  onPress: () => void,
 }
 
-const About = ({ route, navigation, loggedUserId, setIsCourse }: AboutProps) => {
+const About = ({ program, buttonCaption = 'Continuer', onPress }: AboutProps) => {
   const defaultImg = require('../../../../assets/images/authentication_background_image.jpg');
-  const { signOut } = useContext(AuthContext);
-  const { program } = route.params;
   const programImage = get(program, 'image.link') || '';
   const source = programImage ? { uri: programImage } : defaultImg;
-  const subProgram = program.subPrograms ? program.subPrograms[0] : null;
-  const incompleteSteps = subProgram?.steps?.length && subProgram.steps[0].activities?.length
-    ? subProgram.steps.map(st => ({ ...st, activities: st.activities.filter(ac => !ac.activityHistories?.length) }))
-      .filter(st => st.activities.length)
-    : [];
-  const nextActivityId = incompleteSteps.length ? incompleteSteps[0].activities[0]._id : '';
-  const course = subProgram && subProgram.courses ? subProgram.courses[0] : {};
-  const courseId = course._id;
-  const hasAlreadySubscribed = course.trainees.includes(loggedUserId);
+  const navigation = useNavigation();
 
   const hardwareBackPress = () => {
     goBack();
@@ -53,27 +38,7 @@ const About = ({ route, navigation, loggedUserId, setIsCourse }: AboutProps) => 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const goBack = () => navigation.navigate('Home', { screen: 'Explore', params: { screen: 'Catalog' } });
-
-  const goToCourse = () => navigation.navigate('CourseProfile', { courseId });
-
-  const goToNextActivity = () => {
-    navigation.dispatch(StackActions.push('CourseProfile', { courseId }));
-    navigation.navigate('CardContainer', { activityId: nextActivityId, profileId: courseId });
-  };
-
-  const subscribeAndGoToCourseProfile = async () => {
-    try {
-      if (!hasAlreadySubscribed) await Courses.registerToELearningCourse(courseId);
-      setIsCourse(true);
-      if (nextActivityId) goToNextActivity();
-      else goToCourse();
-    } catch (e) {
-      if (e.status === 401) signOut();
-    }
-  };
-
-  const buttonCaption = hasAlreadySubscribed ? 'Continuer' : 'Commencer';
+  const goBack = () => navigation.goBack();
 
   return (
     <ScrollView contentContainerStyle={styles.contentContainer} showsVerticalScrollIndicator={false}>
@@ -100,15 +65,13 @@ const About = ({ route, navigation, loggedUserId, setIsCourse }: AboutProps) => 
           </View>
         }
       </View>
-      <Button style={styles.footer} caption={buttonCaption} onPress={subscribeAndGoToCourseProfile} />
+      <Button style={styles.footer} caption={buttonCaption} onPress={onPress} />
     </ScrollView>
   );
 };
-
-const mapStateToProps = state => ({ loggedUserId: getLoggedUserId(state) });
 
 const mapDispatchToProps = (dispatch: ({ type }: ActionWithoutPayloadType) => void) => ({
   setIsCourse: (isCourse: boolean) => dispatch(CoursesActions.setIsCourse(isCourse)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(About);
+export default connect(null, mapDispatchToProps)(About);
