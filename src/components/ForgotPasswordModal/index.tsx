776829f06fedb45857dbdcb8dde6de056f23c7c5
ulysse <ulysse@alenvi.io>
2021-01-20 17:38:16 +0100

    COM-1813: code verification mail if forgot password

--- src/components/ForgotPasswordModal/index.tsx
+++ src/components/ForgotPasswordModal/index.tsx
@@ -9,6 +9,7 @@ import styles from './styles';
 interface ForgotPasswordModalProps {
   visible: boolean,
   isLoading: boolean,
+  errorMessage: string,
   setModal: (value) => void,
   sendEmail: () => void
 }
@@ -16,6 +17,7 @@ interface ForgotPasswordModalProps {
 const ForgotPasswordModal = ({
   visible,
   isLoading,
+  errorMessage,
   setModal,
   sendEmail,
 }: ForgotPasswordModalProps) => (
@@ -24,14 +26,13 @@ const ForgotPasswordModal = ({
       <View style={styles.modalContent}>
         <FeatherButton name={'x-circle'} onPress={() => setModal(false)} size={ICON.LG} color={GREY[600]}
           style={styles.goBack} />
-        <View style={styles.content}>
-          <Text style={styles.title}>Confirmez votre identité</Text>
-          <Text style={styles.text} >
+        <Text style={styles.title}>Confirmez votre identité</Text>
+        <Text style={styles.text} >
       Pour réinitialiser votre mot de passe, vous devez d’abord confirmer votre identité par un code temporaire.
-          </Text>
-        </View>
+        </Text>
         <NiButton caption='Recevoir le code par e-mail' style={styles.button} onPress={sendEmail} loading={isLoading}
           bgColor={PINK[500]} borderColor={PINK[500]} color={WHITE} />
+        <Text style={styles.unvalid}>{errorMessage}</Text>
       </View>
     </View>
   </Modal>);
