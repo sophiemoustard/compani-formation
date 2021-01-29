31b655720229ba71af12d38c293101cbd220bc84
ulysse <ulysse@alenvi.io>
2021-01-29 10:40:35 +0100

    COM-1811: refacto

--- src/core/data/rules.ts
+++ src/core/data/rules.ts
@@ -1,34 +1,26 @@
 /* eslint-disable max-len */
 
-export const articles = [
-  `Le présent règlement s’applique à toutes les personnes participantes à une action de formation organisée par Compani. Un exemplaire est remis à chaque stagiaire.
+export const articles = {
+  1: `Le présent règlement s’applique à toutes les personnes participantes à une action de formation organisée par Compani. Un exemplaire est remis à chaque stagiaire.
 
 Le règlement définit les règles d’hygiène et de sécurité, les règles générales et permanentes relatives à la discipline ainsi que la nature et l’échelle des sanctions pouvant être prises vis-à-vis des stagiaires qui y contreviennent et les garanties procédurales applicables lorsqu’une sanction est envisagée.
 
 Toute personne doit respecter les termes du présent règlement durant toute la durée de l’action de formation.`,
-
-  `La prévention des risques d’accidents et de maladies est impérative et exige de chacunle respect:
+  2: `La prévention des risques d’accidents et de maladies est impérative et exige de chacunle respect:
 - des prescriptions applicables en matière d’hygiène et de sécurité sur les lieux de formation.
 - de toute consigne imposée soit par la direction de l’organisme de formation soit par le
 constructeur ou le formateur s’agissant notamment de l’usage des matériels mis à disposition.
 
 Chaque stagiaire doit ainsi veiller à sa sécurité personnelle et à celle des autres en respectant, en fonction de sa formation, les consignes générales et particulières en matière d’hygiène et de sécurité. S’il constate un dysfonctionnement du système de sécurité, il en avertit immédiatement la direction de l’organisme de formation. Le non-respect de ces consignes expose la personne à des sanctions disciplinaires.`,
-
-  `Les consignes d’incendie et notamment un plan de localisation des extincteurs et des issues de secours sont affichés dans les locaux de l’organisme de formation. Le stagiaire doit en prendre connaissance.
+  3: `Les consignes d’incendie et notamment un plan de localisation des extincteurs et des issues de secours sont affichés dans les locaux de l’organisme de formation. Le stagiaire doit en prendre connaissance.
 
 En cas d’alerte, le stagiaire doit cesser toute activité de formation et suivre dans le calme les instructions du représentant habilité de l’organisme de formation ou des services de secours.
 
-Tout stagiaire témoin d’un début d’incendie doit immédiatement appeler les secours en composant le 18 à
-partir d’un téléphone fixe ou le 112 à partir d’un téléphone portable et alerter un représentant de l’organisme de
-formation.`,
-
-  'L’introduction ou la consommation de drogue ou de boissons alcoolisées dans les locaux est formellement interdite. Il est interdit aux stagiaires de pénétrer ou de séjourner en état d’ivresse ou sous l’emprise de drogue dans l’organisme de formation. Les stagiaires auront accès lors des pauses aux postes de distribution de boissons non alcoolisées.',
-
-  'Il est formellement interdit de fumer dans les salles de formation et plus généralement dans l’enceinte de l’organisme de formation.',
-
-  'Le stagiaire victime d’un accident - survenu pendant la formation ou pendant le temps de trajet entre le lieu de formation et son domicile ou son lieu de travail – ou le témoin de cet accident avertit immédiatement la direction de l’organisme de formation. Le responsable de l’organisme de formation entreprend les démarches appropriées en matière de soins et réalise la déclaration auprès de la caisse de Sécurité sociale compétente.',
-
-  `Article 7.1. - Horaires de formation
+Tout stagiaire témoin d’un début d’incendie doit immédiatement appeler les secours en composant le 18 à partir d’un téléphone fixe ou le 112 à partir d’un téléphone portable et alerter un représentant de l’organisme de formation.`,
+  4: 'L’introduction ou la consommation de drogue ou de boissons alcoolisées dans les locaux est formellement interdite. Il est interdit aux stagiaires de pénétrer ou de séjourner en état d’ivresse ou sous l’emprise de drogue dans l’organisme de formation. Les stagiaires auront accès lors des pauses aux postes de distribution de boissons non alcoolisées.',
+  5: 'Il est formellement interdit de fumer dans les salles de formation et plus généralement dans l’enceinte de l’organisme de formation.',
+  6: 'Le stagiaire victime d’un accident - survenu pendant la formation ou pendant le temps de trajet entre le lieu de formation et son domicile ou son lieu de travail – ou le témoin de cet accident avertit immédiatement la direction de l’organisme de formation. Le responsable de l’organisme de formation entreprend les démarches appropriées en matière de soins et réalise la déclaration auprès de la caisse de Sécurité sociale compétente.',
+  7: `Article 7.1. - Horaires de formation
 
 Les stagiaires doivent se conformer aux horaires fixés et communiqués au préalable par l’organisme de formation. Le non-respect de ces horaires peut entraîner des sanctions. Sauf circonstances exceptionnelles, les stagiaires ne peuvent s’absenter pendant les heures de stage.
 
@@ -47,20 +39,15 @@ Le stagiaire est tenu de renseigner la feuille d’émargement au fur et à mesu
 A l’issue de l’action de formation, il se voit remettre une attestation de fin de formation et une attestation de présence au stage à transmettre, selon le cas, à son employeur/administration ou à l’organisme qui finance l’action.
 
 Le stagiaire remet, dans les meilleurs délais, à l’organisme de formation les documents qu’il doit renseigner en tant que prestataire (demande de rémunération ou de prise en charges des frais liés à la formation ; attestations d’inscription ou d’entrée en stage...).`,
-
-  `Sauf autorisation expresse de la direction de l’organisme de formation, le stagiaire ne peut :
+  8: `Sauf autorisation expresse de la direction de l’organisme de formation, le stagiaire ne peut :
 -Entrer ou demeurer dans les locaux de formation à d’autres fins que la formation ;
 -Y introduire, faire introduire ou faciliter l’introduction de personnes étrangères à l’organisme ; procéder, dans ces derniers, à la vente de biens ou de services.`,
-
-  'Le stagiaire est invité à se présenter à l’organisme en tenue vestimentaire correcte.',
-
-  'Il est demandé à tout stagiaire d’avoir un comportement garantissant le respect des règles élémentaires de savoir vivre, de savoir être en collectivité et le bon déroulement des formations',
-
-  `Sauf autorisation particulière de la direction de l’organisme de formation, l’usage du matériel de formation se fait sur les lieux de formation et est exclusivement réservé à l’activité de formation. L’utilisation du matériel à des fins personnelles est interdite.
+  9: 'Le stagiaire est invité à se présenter à l’organisme en tenue vestimentaire correcte.',
+  10: 'Il est demandé à tout stagiaire d’avoir un comportement garantissant le respect des règles élémentaires de savoir vivre, de savoir être en collectivité et le bon déroulement des formations',
+  11: `Sauf autorisation particulière de la direction de l’organisme de formation, l’usage du matériel de formation se fait sur les lieux de formation et est exclusivement réservé à l’activité de formation. L’utilisation du matériel à des fins personnelles est interdite.
 
 Le stagiaire est tenu de conserver en bon état le matériel qui lui est confié pour la formation. Il doit en faire un usage conforme à son objet et selon les règles délivrées par le formateur. Le stagiaire signale immédiatement au formateur toute anomalie du matériel.`,
-
-  `Tout manquement du stagiaire à l’une des prescriptions du présent règlement intérieur pourra faire l’objet d’une sanction prononcée par le responsable de l’organisme de formation ou son représentant. Tout agissement considéré comme fautif pourra, en fonction de sa nature et de sa gravité, faire l’objet de l’une ou l’autre des sanctions suivantes :
+  12: `Tout manquement du stagiaire à l’une des prescriptions du présent règlement intérieur pourra faire l’objet d’une sanction prononcée par le responsable de l’organisme de formation ou son représentant. Tout agissement considéré comme fautif pourra, en fonction de sa nature et de sa gravité, faire l’objet de l’une ou l’autre des sanctions suivantes :
 -Rappel à l’ordre
 -Avertissement écrit par le directeur de l’organisme de formation ou par son représentant
 -Blâme
@@ -70,8 +57,7 @@ Le stagiaire est tenu de conserver en bon état le matériel qui lui est confié
 Les amendes ou autres sanctions pécuniaires sont interdites.
 
 Le responsable de l’organisme de formation ou son représentant informe de la sanction prise : l’employeur du salarié stagiaire ou l’administration de l’agent stagiaire et/ou le financeur du stage.`,
-
-  `Article 13.1. – Information du stagiaire
+  13: `Article 13.1. – Information du stagiaire
 
 Aucune sanction ne peut être infligée au stagiaire sans que celui-ci ait été informé au préalable des griefs retenus contre lui.
 
@@ -93,14 +79,11 @@ Article 13.4. - Prononcé de la sanction
 La sanction ne peut intervenir moins d’un jour franc ni plus de quinze jours après l’entretien.
 
 La sanction fait l’objet d’une notification écrite et motivée au stagiaire sous forme d’une lettre recommandée ou remise contre décharge.`,
-
-  `Dans les stages d’une durée supérieure à 500 heures, il est procédé simultanément à l’élection d’un délégué titulaire et d’un délégué suppléant au scrutin uninominal à deux tours, selon les modalités suivantes :
+  14: `Dans les stages d’une durée supérieure à 500 heures, il est procédé simultanément à l’élection d’un délégué titulaire et d’un délégué suppléant au scrutin uninominal à deux tours, selon les modalités suivantes :
   
 Tous les stagiaires sont électeurs et éligibles sauf les détenus. Le scrutin a lieu, pendant les heures de la formation, au plus tôt 20 heures et au plus tard 40 heures après le début du stage.
 
 Le responsable de l’organisme de formation a la charge de l’organisation du scrutin. Il en assure le bon déroulement. Il adresse un procès-verbal de carence, transmis au préfet de région territorialement compétent, lorsque la représentation des stagiaires ne peut être assurée.`,
-
-  'Les délégués sont élus pour la durée du stage. Leurs fonctions prennent fin lorsqu’ils cessent, pour quelque cause que ce soit, de participer au stage. Si le délégué titulaireet le délégué suppléant ont cessé leurs fonctions avant la fin du stage, il est procédé à une nouvelle élection.',
-
-  'Les délégués font toute suggestion pour améliorer le déroulement des stages et les conditions de vie des stagiaires dans l’organisme de formation. Ils présentent toutes les réclamations individuelles ou collectives relatives à ces matières, aux conditions d’hygiène et de sécurité et à l’application du règlement intérieur.',
-];
+  15: 'Les délégués sont élus pour la durée du stage. Leurs fonctions prennent fin lorsqu’ils cessent, pour quelque cause que ce soit, de participer au stage. Si le délégué titulaireet le délégué suppléant ont cessé leurs fonctions avant la fin du stage, il est procédé à une nouvelle élection.',
+  16: 'Les délégués font toute suggestion pour améliorer le déroulement des stages et les conditions de vie des stagiaires dans l’organisme de formation. Ils présentent toutes les réclamations individuelles ou collectives relatives à ces matières, aux conditions d’hygiène et de sécurité et à l’application du règlement intérieur.',
+};
