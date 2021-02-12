53f932464b6b2732d4a82fe81eb763f261db2020
ulysse <ulysse@alenvi.io>
2021-02-12 14:18:52 +0100

    COM-1861: zoom image but can't move on image

--- src/screens/courses/cardTemplates/TitleTextMediaCard/index.tsx
+++ src/screens/courses/cardTemplates/TitleTextMediaCard/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Text, Image, ScrollView } from 'react-native';
+import { Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import Markdown from 'react-native-markdown-display';
 import CardHeader from '../../../../components/cards/CardHeader';
 import CardFooter from '../../../../components/cards/CardFooter';
+import ZoomImage from '../../../../components/ZoomImage';
 import Selectors from '../../../../store/activities/selectors';
 import cardsStyle from '../../../../styles/cards';
 import { markdownStyle } from '../../../../styles/common';
@@ -27,6 +28,7 @@ const TitleTextMediaCard = ({ card, index, isLoading, setIsSwipeEnabled }: Title
   const [mediaHeight, setMediaHeight] = useState<number>(CARD_MEDIA_MAX_HEIGHT);
   const [mediaType, setMediaType] = useState<string>('');
   const [mediaSource, setMediaSource] = useState<{ uri: string } | undefined>();
+  const [zoomImage, setZoomImage] = useState<boolean>(false);
 
   useEffect(() => setIsSwipeEnabled(true));
 
@@ -53,12 +55,16 @@ const TitleTextMediaCard = ({ card, index, isLoading, setIsSwipeEnabled }: Title
         <Text style={cardsStyle.title}>{card.title}</Text>
         <Markdown style={markdownStyle(cardsStyle.text)}>{card.text}</Markdown>
         {mediaType === IMAGE && !!mediaSource &&
-          <Image source={mediaSource} style={[cardsStyle.media, styleWithHeight.media]} />}
+          <TouchableOpacity onPress={() => setZoomImage(true)}>
+            <Image source={mediaSource} style={[cardsStyle.media, styleWithHeight.media]} />
+          </TouchableOpacity>}
         {mediaType === VIDEO && !!mediaSource && <NiVideo mediaSource={mediaSource} />}
         {mediaType === AUDIO && !!mediaSource && <NiAudio mediaSource={mediaSource}/>}
       </ScrollView>
       <FooterGradient />
       <CardFooter index={index} />
+      {zoomImage &&
+        <ZoomImage image={mediaSource} setZoomImage={setZoomImage} mediaHeight={mediaHeight} />}
     </>
   );
 };
