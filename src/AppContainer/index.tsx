f17b8455ba17752fed46cfe6d6010f6fb31dc089
Chloe Molle <chloe.molle@neuf.fr>
2021-02-17 15:07:04 +0100

    COM-1913 format code

--- src/AppContainer/index.tsx
+++ src/AppContainer/index.tsx
@@ -108,8 +108,10 @@ const AppContainer = ({ setLoggedUser, statusBarVisible }: AppContainerProps) =>
       </View>
       <MainStack.Navigator screenOptions={{ headerShown: false }}>
         {Object.entries(alenviToken ? userScreens : authScreens)
-          .map(([name, component]) => (<MainStack.Screen key={name} name={name} component={component}
-            options={undismissableScreens.includes(name) ? { gestureEnabled: false } : {}} />))}
+          .map(([name, component]) => (
+            <MainStack.Screen key={name} name={name} component={component}
+              options={undismissableScreens.includes(name) ? { gestureEnabled: false } : {}} />
+          ))}
       </MainStack.Navigator>
     </NavigationContainer>
   );
