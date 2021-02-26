65b9195a727688fbbb1e4628079d3fcd70af759f
Sophie Moustard <sophie.moustard@gmail.com>
2021-02-26 10:33:46 +0100

    COM-1899 - merge style

--- src/components/CreateAccountForm/styles.ts
+++ src/components/CreateAccountForm/styles.ts
@@ -1,28 +1,9 @@
 import { StyleSheet } from 'react-native';
 import { MARGIN } from '../../styles/metrics';
-import { FIRA_SANS_BOLD, FIRA_SANS_ITALIC } from '../../styles/fonts';
+import { FIRA_SANS_ITALIC } from '../../styles/fonts';
 import { GREY } from '../../styles/colors';
 
-const styles = StyleSheet.create({
-  title: {
-    ...FIRA_SANS_BOLD.LG,
-    marginVertical: MARGIN.LG,
-  },
-  keyboardAvoidingView: {
-    flexGrow: 1,
-  },
-  input: {
-    marginBottom: MARGIN.XS,
-  },
-  container: {
-    flexGrow: 1,
-    marginHorizontal: MARGIN.LG,
-  },
-  footer: {
-    justifyContent: 'flex-end',
-    marginBottom: MARGIN.MD,
-    flex: 1,
-  },
+export default StyleSheet.create({
   modalText: {
     ...FIRA_SANS_ITALIC.SM,
     color: GREY[600],
@@ -32,5 +13,3 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
   },
 });
-
-export default styles;
--- src/screens/EmailForm/styles.ts
+++ src/screens/EmailForm/styles.ts
@@ -1,31 +1,9 @@
 import { StyleSheet } from 'react-native';
 import { MARGIN } from '../../styles/metrics';
-import { FIRA_SANS_BOLD } from '../../styles/fonts';
 
-const styles = footerTooClose => StyleSheet.create({
-  title: {
-    ...FIRA_SANS_BOLD.LG,
-    marginVertical: MARGIN.LG,
-  },
-  keyboardAvoidingView: {
-    flex: 1,
-  },
-  container: {
-    flexGrow: 1,
-    marginHorizontal: MARGIN.LG,
-  },
+export default StyleSheet.create({
   goBack: {
     marginTop: MARGIN.MD,
     marginLeft: MARGIN.MD,
   },
-  input: {
-    marginBottom: MARGIN.XS,
-  },
-  footer: {
-    marginBottom: footerTooClose ? MARGIN.MD : MARGIN.XL,
-    justifyContent: 'flex-end',
-    flex: 1,
-  },
 });
-
-export default styles;
