da8abf1876b5fee4468d989a5ccf9831dfa68658
Sophie Moustard <sophie.moustard@gmail.com>
2021-04-19 14:00:27 +0200

    useCallback in crateAccountForm

--- src/screens/CreateAccount/index.tsx
+++ src/screens/CreateAccount/index.tsx
@@ -18,12 +18,39 @@ interface CreateAccountProps {
   navigation: NavigationType,
 }
 
+type CreateAccountDataType = {
+  type: string,
+  field: string,
+  title: string,
+  caption: string,
+  value: string,
+  isValid: boolean,
+  errorMessage: string,
+  isValidationAttempted: boolean,
+  required: boolean,
+  openModal?: { text: string, link: string },
+}
+
+const formatCreationPayload = (formList: CreateAccountDataType[][], email) => {
+  const data = {
+    identity: formList[0][0].value === ''
+      ? { lastname: formList[1][0].value }
+      : { lastname: formList[1][0].value, firstname: formList[0][0].value },
+    local: { email, password: formList[3][0].value },
+  };
+
+  return Object.assign(
+    data,
+    formList[2][0].value === '' ? null : { contact: { phone: formatPhoneForPayload(formList[2][0].value) } }
+  );
+};
+
 const CreateAccount = ({ route, navigation }: CreateAccountProps) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { email } = route.params;
   const { signIn, signOut } = useContext(AuthContext);
-  const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
-  const [formList, setFormList] = useState([
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [formList, setFormList] = useState<CreateAccountDataType[][]>([
     [{
       type: 'text',
       field: 'firstname',
@@ -87,28 +114,13 @@ const CreateAccount = ({ route, navigation }: CreateAccountProps) => {
     : navigation.navigate('EmailForm', { firstConnection: true }));
 
   const setForm = (data, index) => {
-    setFormList(prevFormList => (prevFormList
-      .map((fieldsGroup, i) => (i === index ? data : fieldsGroup))));
-  };
-
-  const formatCreationPayload = () => {
-    const data = {
-      identity: formList[0][0].value === ''
-        ? { lastname: formList[1][0].value }
-        : { lastname: formList[1][0].value, firstname: formList[0][0].value },
-      local: { email, password: formList[3][0].value },
-    };
-
-    return Object.assign(
-      data,
-      formList[2][0].value === '' ? null : { contact: { phone: formatPhoneForPayload(formList[2][0].value) } }
-    );
+    setFormList(prevFormList => (prevFormList.map((fieldsGroup, i) => (i === index ? data : fieldsGroup))));
   };
 
   const create = async () => {
     try {
       setIsLoading(true);
-      await Users.create(formatCreationPayload());
+      await Users.create(formatCreationPayload(formList, email));
       signIn({ email, password: formList[3][0].value });
     } catch (e) {
       if (e.status === 401) signOut();
@@ -117,28 +129,30 @@ const CreateAccount = ({ route, navigation }: CreateAccountProps) => {
     }
   };
 
+  const toggleModal = () => setIsModalVisible(isVisible => !isVisible);
+
   const renderScreen = (fields: Array<any>, i: number) => (
-    <Stack.Screen key={fields[0].title} name={`create-account-screen-${i}`}>
-      {() => (
-        <>
-          <View style={styles.header}>
-            <FeatherButton name='arrow-left' onPress={() => goBack(i)} size={ICON.MD} color={GREY[600]}
-              disabled={isLoading} />
-            <ProgressBar progress={((i + 1) / formList.length) * 100} />
-          </View>
-          <CreateAccountForm isLoading={isLoading} data={fields} setData={setForm} index={i} goBack={goBack}
-            create={create} openModal={() => setIsModalOpened(true)} />
-          <TermoOfUseModal visible={isModalOpened} onRequestClose={() => setIsModalOpened(false)} />
-        </>
-      )}
-    </Stack.Screen>
+    <>
+      <View style={styles.header}>
+        <FeatherButton name='arrow-left' onPress={() => goBack(i)} size={ICON.MD} color={GREY[600]}
+          disabled={isLoading} />
+        <ProgressBar progress={((i + 1) / formList.length) * 100} />
+      </View>
+      <CreateAccountForm isLoading={isLoading} data={fields} setData={setForm} index={i} goBack={goBack}
+        create={create} openModal={toggleModal} />
+      <TermoOfUseModal visible={isModalVisible} onRequestClose={toggleModal} />
+    </>
   );
 
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {formList.map((fields, i) => renderScreen(fields, i))}
+      {formList.map((fields, i) => (
+        <Stack.Screen key={fields[0].title} name={`create-account-screen-${i}`}>
+          {() => renderScreen(fields, i)}
+        </Stack.Screen>
+      ))}
     </Stack.Navigator>
   );
 };
