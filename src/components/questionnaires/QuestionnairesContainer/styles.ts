aa30d175543d330b0289468f3b915130b3ad7e15
KennyCallegari <kenny@alenvi.io>
2021-04-19 17:35:56 +0200

    COM-1995 - Fix Romain's review

--- src/components/questionnaires/QuestionnairesContainer/styles.ts
+++ src/components/questionnaires/QuestionnairesContainer/styles.ts
@@ -1,21 +1,23 @@
 import { StyleSheet } from 'react-native';
-import { YELLOW } from '../../../styles/colors';
+import { GREY, YELLOW } from '../../../styles/colors';
 import { FIRA_SANS_ITALIC, FIRA_SANS_BOLD_ITALIC } from '../../../styles/fonts';
-import { MARGIN, PADDING } from '../../../styles/metrics';
+import { BORDER_WIDTH, MARGIN, PADDING } from '../../../styles/metrics';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: YELLOW[100],
     marginBottom: MARGIN.LG,
+    borderColor: GREY[200],
+    borderBottomWidth: BORDER_WIDTH,
   },
   header: {
     backgroundColor: YELLOW[300],
     flexDirection: 'row',
+    paddingHorizontal: PADDING.LG,
+    paddingVertical: PADDING.MD,
   },
   headerText: {
     ...FIRA_SANS_ITALIC.SM,
-    paddingHorizontal: PADDING.LG,
-    paddingVertical: PADDING.MD,
   },
   questionnairesCount: {
     ...FIRA_SANS_BOLD_ITALIC.SM,
