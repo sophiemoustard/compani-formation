3fa625f5ffba7edded7cd23043c3823d4ba18176
KennyCallegari <kenny@alenvi.io>
2021-04-29 11:05:07 +0200

    COM-2054 - Add helper for notification

--- App.tsx
+++ App.tsx
@@ -67,7 +67,7 @@ Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldSetBadge: true,
   }),
 });
 
