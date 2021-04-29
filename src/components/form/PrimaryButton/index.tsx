d7401b9d6ea75938b50c408ca8086dbead75ab44
Sophie Moustard <sophie.moustard@gmail.com>
2021-04-29 15:01:14 +0200

    COM-2018 - merge index and custom style

--- src/components/form/PrimaryButton/index.tsx
+++ src/components/form/PrimaryButton/index.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { TouchableOpacity, Text, ActivityIndicator } from 'react-native';
-import commonStyle from '../../../styles/common';
+import React from 'react';
+import NiButton from '../Button';
 import { PINK, WHITE } from '../../../styles/colors';
-import styles from './styles';
 
 interface PrimaryButtonProps {
   customStyle?: Object,
@@ -14,28 +12,17 @@ interface PrimaryButtonProps {
   disabled?: boolean,
 }
 
-const PrimaryButton = (
-  {
-    customStyle,
-    caption,
-    onPress,
-    loading = false,
-    bgColor = PINK[500],
-    color = WHITE,
-    disabled = false,
-  }: PrimaryButtonProps
-) => {
-  const [style, setStyle] = useState<{ button: object, textButton: object}>(styles(bgColor, color));
-
-  useEffect(() => { setStyle(styles(bgColor, color)); }, [bgColor, color]);
-
-  return (
-    <TouchableOpacity style={[style.button, customStyle]} onPress={onPress} disabled={loading || disabled}
-      testID={caption}>
-      {!loading && <Text style={{ ...style.textButton, color }}>{caption}</Text>}
-      {loading && <ActivityIndicator style={commonStyle.disabled} color={color} size="small" />}
-    </TouchableOpacity>
-  );
-};
+const PrimaryButton = ({
+  customStyle,
+  caption,
+  onPress,
+  loading = false,
+  bgColor = PINK[500],
+  color = WHITE,
+  disabled = false,
+}: PrimaryButtonProps) => (
+  <NiButton customStyle={customStyle} caption={caption} onPress={onPress} loading={loading} disabled={disabled}
+    bgColor={bgColor} borderColor={bgColor} color={color} />
+);
 
 export default PrimaryButton;
