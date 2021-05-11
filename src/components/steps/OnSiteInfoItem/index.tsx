f3e75e81d3fa366beb66b81df3c4a59942c342d5
Sophie Moustard <sophie.moustard@gmail.com>
2021-05-11 10:41:57 +0200

    Refacto onsitecellInfomodal

--- src/components/steps/OnSiteInfoItem/index.tsx
+++ src/components/steps/OnSiteInfoItem/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, Linking, TouchableOpacity } from 'react-native';
+import get from 'lodash/get';
 import { CourseSlotType } from '../../../types/CourseSlotType';
 import moment from '../../../core/helpers/moment';
 import OnSiteHoursDisplay from '../OnSiteHoursDisplay';
@@ -9,23 +10,25 @@ interface OnSiteInfoItemProps {
   info: { slots: Array<CourseSlotType> },
 }
 
+const openMaps = async add => Linking.openURL(`https://www.google.com/maps/search/?api=1&query=${add}`);
+
+const hoursItem = slot => <OnSiteHoursDisplay startDate={slot.startDate} endDate={slot.endDate} />;
+
 const OnSiteInfoItem = ({ info }: OnSiteInfoItemProps) => {
-  const [address] = useState(info?.slots[0]?.address?.fullAddress);
+  const [address, setAddress] = useState<string>('');
 
-  const openMaps = async add => Linking.openURL(`https://www.google.com/maps/search/?api=1&query=${add}`);
-
-  const hoursItem = slot => <OnSiteHoursDisplay startDate={slot.startDate} endDate={slot.endDate} />;
+  useEffect(() => { setAddress(get(info, 'slots[0].address.fullAddress') || ''); }, [info]);
 
   return (
-    <View>
+    <>
       <Text style={styles.date}>{moment(info.slots[0].startDate).format('dddd Do MMMM')}</Text>
       <FlatList horizontal ItemSeparatorComponent={() => <View style={styles.separator} />} data={info.slots}
         keyExtractor={item => item._id} renderItem={({ item }) => hoursItem(item)} />
-      {address && (
+      {!!address && (
         <TouchableOpacity onPress={() => openMaps(address)}>
           <Text style={styles.address}>{address}</Text>
         </TouchableOpacity>)}
-    </View>
+    </>
   );
 };
 
