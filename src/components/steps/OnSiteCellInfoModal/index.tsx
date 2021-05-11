93c6ad55136c20578bbf537d3f2133db068ec701
Sophie Moustard <sophie.moustard@gmail.com>
2021-05-11 10:41:57 +0200

    Refacto onsitecellInfomodal

--- src/components/steps/OnSiteCellInfoModal/index.tsx
+++ src/components/steps/OnSiteCellInfoModal/index.tsx
@@ -16,32 +16,30 @@ interface OnSiteCellInfoModalProps {
   onRequestClose: () => void,
 }
 
-const OnSiteCellInfoModal = ({ visible, title, stepSlots, onRequestClose }: OnSiteCellInfoModalProps) => {
-  const formatStepSlotsForFlatList = (slots) => {
-    const formattedSlots = slots.reduce((acc, slot) => {
-      const startDate = moment(slot.startDate).format('DD/MM/YYYY');
-      if (acc[startDate]) acc[startDate].push(slot);
-      else acc[startDate] = [slot];
+const formatStepSlotsForFlatList = (slots) => {
+  const formattedSlots = slots.reduce((acc, slot) => {
+    const startDate = moment(slot.startDate).format('DD/MM/YYYY');
+    if (acc[startDate]) acc[startDate].push(slot);
+    else acc[startDate] = [slot];
 
-      return acc;
-    },
-    {});
+    return acc;
+  },
+  {});
 
-    return Object.keys(formattedSlots).map(key => ({ startDate: key, slots: formattedSlots[key] }));
-  };
-
-  return (
-    <NiModal visible={visible}>
-      <View style={styles.header}>
-        <Text style={styles.title} lineBreakMode={'tail'} numberOfLines={3}>{title}</Text>
-        <FeatherButton name='x-circle' onPress={onRequestClose} size={ICON.LG} color={GREY[500]}
-          style={styles.closeButton} />
-      </View>
-      <FlatList ItemSeparatorComponent={() => <View style={styles.stepInfoSeparator} />} scrollEnabled={true}
-        data={formatStepSlotsForFlatList(stepSlots)} renderItem={({ item }) => <OnSiteInfoItem info={item} />}
-        keyExtractor={item => item.startDate} />
-    </NiModal>
-  );
+  return Object.keys(formattedSlots).map(key => ({ startDate: key, slots: formattedSlots[key] }));
 };
+
+const OnSiteCellInfoModal = ({ visible, title, stepSlots, onRequestClose }: OnSiteCellInfoModalProps) => (
+  <NiModal visible={visible}>
+    <View style={styles.header}>
+      <Text style={styles.title} lineBreakMode={'tail'} numberOfLines={3}>{title}</Text>
+      <FeatherButton name='x-circle' onPress={onRequestClose} size={ICON.LG} color={GREY[500]}
+        style={styles.closeButton} />
+    </View>
+    <FlatList ItemSeparatorComponent={() => <View style={styles.stepInfoSeparator} />} scrollEnabled={true}
+      data={formatStepSlotsForFlatList(stepSlots)} renderItem={({ item }) => <OnSiteInfoItem info={item} />}
+      keyExtractor={item => item.startDate} />
+  </NiModal>
+);
 
 export default OnSiteCellInfoModal;
