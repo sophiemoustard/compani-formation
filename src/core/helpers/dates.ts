01f5c43c97ea7e5165bab7ee416992068dff62b4
CorentinMartinot <corentin.martinot@gmail.com>
2021-08-10 16:57:34 +0200

    COM-2260 fix android bug : Intl is not supported by facebook/hermes and to be imported via format.js

--- src/core/helpers/dates.ts
+++ src/core/helpers/dates.ts
@@ -1,4 +1,16 @@
-import { DateTime, Settings, DurationObjectUnits } from 'luxon';
+import { DateTime, DurationObjectUnits, Settings } from 'luxon';
+import '@formatjs/intl-getcanonicallocales/polyfill';
+
+import '@formatjs/intl-locale/polyfill';
+
+import '@formatjs/intl-pluralrules/polyfill';
+import '@formatjs/intl-pluralrules/locale-data/fr';
+
+import '@formatjs/intl-numberformat/polyfill';
+import '@formatjs/intl-numberformat/locale-data/fr';
+
+import '@formatjs/intl-datetimeformat/polyfill';
+import '@formatjs/intl-datetimeformat/locale-data/fr';
 
 Settings.defaultLocale = 'fr';
 
