046fad1fea33592dbf7ea3871783878c61f44f7d
CorentinMartinot <corentin.martinot@gmail.com>
2021-08-12 19:28:59 +0200

    COM-2260 change class format to factory fct format

--- src/core/helpers/dates.ts
+++ src/core/helpers/dates.ts
@@ -1,4 +1,4 @@
-import { Settings } from 'luxon';
+import { Settings, DateTime, DurationObjectUnits } from 'luxon';
 import '@formatjs/intl-getcanonicallocales/polyfill';
 import '@formatjs/intl-locale/polyfill';
 import '@formatjs/intl-pluralrules/polyfill';
@@ -7,8 +7,79 @@ import '@formatjs/intl-numberformat/polyfill';
 import '@formatjs/intl-numberformat/locale-data/fr';
 import '@formatjs/intl-datetimeformat/polyfill';
 import '@formatjs/intl-datetimeformat/locale-data/fr';
-import CompaniDate from '../classes/CompaniDate';
 
 Settings.defaultLocale = 'fr';
 
-export const companiDate = (...args: any[]) => new CompaniDate(...args);
+type CompaniDate = {
+  _date: DateTime;
+}
+
+export const companiDate = (...args: any[]) => {
+  const instancedDateTime = instantiateDateTimeFromMisc(...args);
+
+  return companiDateFactory(instancedDateTime);
+};
+
+const companiDateFactory = (_date: DateTime) => ({
+  _date,
+
+  _getDate() {
+    return this._date;
+  },
+  isValid() {
+    return this._date.isValid;
+  },
+  toString() {
+    return this._date.toString() || '';
+  },
+  toISOString() {
+    return this._date.toUTC().toISO() || '';
+  },
+  format(fmt: string) {
+    return this._date.toFormat(fmt) || '';
+  },
+  isSameOrBefore(miscTypeOtherDate : Date | CompaniDate | string) {
+    const otherDate = instantiateDateTimeFromMisc(miscTypeOtherDate);
+
+    return this._date <= otherDate || false;
+  },
+  isBefore(miscTypeOtherDate : Date | CompaniDate | string) {
+    const otherDate = instantiateDateTimeFromMisc(miscTypeOtherDate);
+
+    return this._date < otherDate || false;
+  },
+  diff(miscTypeOtherDate : Date | CompaniDate, unit: keyof DurationObjectUnits, typeFloat = false) {
+    const otherDate = instantiateDateTimeFromMisc(miscTypeOtherDate);
+    const floatDiff = this._date.diff(otherDate, unit).as(unit);
+    const roundedDiff = floatDiff > 0 ? Math.floor(floatDiff) : Math.ceil(floatDiff);
+
+    return typeFloat ? floatDiff : roundedDiff;
+  },
+  endOf(unit: keyof DurationObjectUnits) {
+    const res = this._date.endOf(unit);
+
+    return companiDateFactory(res);
+  },
+  add(amount: number, unit: string) {
+    const res = this._date.plus({ [unit]: amount });
+
+    return companiDateFactory(res);
+  },
+});
+
+const instantiateDateTimeFromMisc = (...args: any[]) => {
+  if (!args.length) {
+    return DateTime.now();
+  } if (args.length === 1 && args[0] instanceof Object && !!args[0]._date && args[0]._date instanceof DateTime) {
+    return args[0]._getDate();
+  } if (args.length === 1 && args[0] instanceof DateTime) {
+    return args[0];
+  } if (args.length === 1 && args[0] instanceof Date) {
+    return DateTime.fromJSDate(args[0]);
+  } if (args.length === 1 && typeof args[0] === 'string') {
+    return DateTime.fromISO(args[0]);
+  } if (args.length === 2 && typeof args[0] === 'string' && typeof args[1] === 'string') {
+    return DateTime.fromFormat(args[0], args[1]);
+  }
+  return DateTime.invalid('wrong arguments');
+};
