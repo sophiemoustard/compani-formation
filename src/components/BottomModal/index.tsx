5be76911392fcaaa27b33a91df8d098c9ac4546d
ulysse <ulysse@alenvi.io>
2021-09-01 15:40:07 +0200

    COM-2306: bottom modal

---
+++ src/components/BottomModal/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Modal } from 'react-native';
+import FeatherButton from '../icons/FeatherButton';
+import { ICON } from '../../styles/metrics';
+import { GREY } from '../../styles/colors';
+import styles from './styles';
+
+interface NiBottomModalProps {
+  visible: boolean,
+  children: any,
+  onRequestClose: () => void,
+}
+
+const BottomModal = ({ visible, children, onRequestClose }: NiBottomModalProps) => (
+  <Modal visible={visible} transparent={true} onRequestClose={onRequestClose}>
+    <View style={styles.modalContainer}>
+      <View style={styles.modalContent}>
+        <FeatherButton name='x-circle' onPress={onRequestClose} size={ICON.LG} color={GREY[600]}
+          style={styles.goBack} />
+        {children}
+      </View>
+    </View>
+  </Modal>);
+
+export default BottomModal;
