c2f8f2a7f3ecfc0e36877461984c89bcdd7a0292
CorentinMartinot <corentin.martinot@gmail.com>
2021-10-28 11:50:02 +0200

    COM-2082 NiModal

--- src/components/MaintenanceModal/index.tsx
+++ src/components/MaintenanceModal/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
-import Modal from '../Modal';
+import NiModal from '../Modal';
 import styles from './styles';
 
 const MaintenanceModal = () => (
-  <Modal visible={true}>
-    <>
-      <Text style={styles.title}> L&apos;application est en maintenance !</Text>
-      <Text style={styles.body}>Elle sera de nouveau disponible dans quelques minutes.</Text>
-    </>
-  </Modal>
+  <NiModal visible={true}>
+    <Text style={styles.title}> L&apos;application est en maintenance !</Text>
+    <Text style={styles.body}>Elle sera de nouveau disponible dans quelques minutes.</Text>
+  </NiModal>
 );
 
 export default MaintenanceModal;
