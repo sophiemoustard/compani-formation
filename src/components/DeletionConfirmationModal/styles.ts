97ecc0bdbcecd26ee6c21ee61ff5b0c584488dc7
ulysse <ulysse@alenvi.io>
2022-05-10 16:00:32 +0200

    COM-2818: center modal

--- src/components/DeletionConfirmationModal/styles.ts
+++ src/components/DeletionConfirmationModal/styles.ts
@@ -6,7 +6,7 @@ import { FIRA_SANS_BOLD, FIRA_SANS_REGULAR } from '../../styles/fonts';
 const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginTop: MARGIN.SM,
     paddingHorizontal: PADDING.MD,
   },
