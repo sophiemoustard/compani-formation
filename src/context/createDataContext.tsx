6462f98a1ca90f9f8c37063d44cf82d04a01874d
ulysse <ulysse@alenvi.io>
2022-07-28 12:32:08 +0200

    COM-2929: destructure ReactNode

--- src/context/createDataContext.tsx
+++ src/context/createDataContext.tsx
@@ -1,14 +1,13 @@
-import { useReducer, createContext, ContextType } from 'react';
-import * as React from 'react';
+import { useReducer, createContext, ContextType, ReactNode } from 'react';
 import { StateType } from './AuthContext';
 
 export interface createDataContextType {
   Context: ContextType<any>,
-  Provider: (children: {children: React.ReactNode}) => JSX.Element
+  Provider: (children: {children: ReactNode}) => JSX.Element
 }
 
 export default (reducer: (state: StateType, actions) => StateType, actions, defaultValue): createDataContextType => {
-  const Provider = ({ children }: {children: React.ReactNode}) => {
+  const Provider = ({ children }: {children: ReactNode}) => {
     const [{ companiToken, loading, error, errorMessage, appIsReady },
       dispatch] = useReducer(reducer, defaultValue);
     const state = { companiToken, loading, error, errorMessage, appIsReady };
