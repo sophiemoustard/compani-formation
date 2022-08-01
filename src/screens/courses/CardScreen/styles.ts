899ee28ab724f4143517e1a54a232ae262bceca5
Palin Manon <palinmanon@MacBook-Pro-de-Palin.local>
2022-08-01 15:39:48 +0200

    COM-2924 - use safeAreaView on card screen

--- src/screens/courses/CardScreen/styles.ts
+++ src/screens/courses/CardScreen/styles.ts
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
-import { GREY } from '../../../styles/colors';
+import { GREY, WHITE } from '../../../styles/colors';
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: WHITE,
+  },
   cardScreen: {
     display: 'flex',
     flex: 1,
