49d01678f3e045ca556d6f4a202d846a45412444
Palin Manon <palinmanon@MacBook-Pro-de-Palin.local>
2022-08-01 16:40:51 +0200

    COM-2924 - use safeAreaView on PasswordReset screen

---
+++ src/screens/PasswordReset/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { WHITE } from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: WHITE,
+  },
+});
+
+export default styles;
