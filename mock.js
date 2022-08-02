d04b98532e7faa2b7016be80a2337fc6b98c947c
Palin Manon <palinmanon@MacBook-Pro-de-Palin.local>
2022-08-02 15:44:28 +0200

    fix integration tests

--- mock.js
+++ mock.js
@@ -1,4 +1,6 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
