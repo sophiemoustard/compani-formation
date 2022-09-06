50a0ff681eb4955d9b08657e63c5b1e8be7997de
CorentinMartinot <corentin.martinot@gmail.com>
2022-09-06 14:11:13 +0200

    COM-2974 remove useless props

--- src/screens/courses/list/TrainerCourses/index.tsx
+++ src/screens/courses/list/TrainerCourses/index.tsx
@@ -1,44 +1,19 @@
 import 'array-flat-polyfill';
 import { Text, View, ScrollView, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { connect } from 'react-redux';
-import { CompositeScreenProps } from '@react-navigation/native';
-import { StackScreenProps } from '@react-navigation/stack';
-import { getLoggedUserId } from '../../../../store/main/selectors';
-import CoursesActions from '../../../../store/courses/actions';
 import commonStyles from '../../../../styles/common';
-import { RootBottomTabParamList, RootStackParamList } from '../../../../types/NavigationType';
-import { ActionWithoutPayloadType } from '../../../../types/store/StoreType';
 import styles from '../styles';
 
-interface TrainerCoursesProps extends CompositeScreenProps<
-StackScreenProps<RootBottomTabParamList>,
-StackScreenProps<RootStackParamList>
-> {
-  setIsCourse: (value: boolean) => void,
-  loggedUserId: string | null,
-}
+const TrainerCourses = () => (
+  <SafeAreaView style={commonStyles.container} edges={['top']}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={commonStyles.title} testID='header'>Espace intervenant</Text>
+      <View style={styles.footer}>
+        <Image style={styles.elipse} source={require('../../../../../assets/images/log_out_background.png')} />
+        <Image source={require('../../../../../assets/images/pa_aidant_balade_bleu.png')} style={styles.fellow} />
+      </View>
+    </ScrollView>
+  </SafeAreaView>
+);
 
-const TrainerCourses = ({ setIsCourse, navigation, loggedUserId }: TrainerCoursesProps) => {
-  console.log('Ceci est temporaire');
-
-  return (
-    <SafeAreaView style={commonStyles.container} edges={['top']}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text style={commonStyles.title} testID='header'>Espace intervenant</Text>
-        <View style={styles.footer}>
-          <Image style={styles.elipse} source={require('../../../../../assets/images/log_out_background.png')} />
-          <Image source={require('../../../../../assets/images/pa_aidant_balade_bleu.png')} style={styles.fellow} />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const mapStateToProps = state => ({ loggedUserId: getLoggedUserId(state) });
-
-const mapDispatchToProps = (dispatch: ({ type }: ActionWithoutPayloadType) => void) => ({
-  setIsCourse: (isCourse: boolean) => dispatch(CoursesActions.setIsCourse(isCourse)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TrainerCourses);
+export default TrainerCourses;
