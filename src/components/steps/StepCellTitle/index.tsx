d016ea5553fbe1d2afd5879fd54080ca29d2f0b4
CorentinMartinot <corentin.martinot@gmail.com>
2022-09-16 09:26:13 +0200

    COM-2975-3 add misc to next step

--- src/components/steps/StepCellTitle/index.tsx
+++ src/components/steps/StepCellTitle/index.tsx
@@ -8,16 +8,18 @@ type StepCellTitleProps = {
   name: StepType['name'],
   type: StepType['type'],
   index: number,
+  misc?: string,
   theoreticalHours?: number,
 }
 
-const StepCellTitle = ({ name, type, index, theoreticalHours = 0 }: StepCellTitleProps) => (
+const StepCellTitle = ({ name, type, index, misc, theoreticalHours = 0 }: StepCellTitleProps) => (
   <View style={styles.textContainer}>
     <Text style={styles.stepType}>
       {`ÉTAPE ${index + 1} - ${stepTypeOptions[type]}`}
       {type === E_LEARNING && !!theoreticalHours && ` (${formatDuration(theoreticalHours)})`}
     </Text>
     <Text lineBreakMode={'tail'} numberOfLines={2} style={styles.stepName}>{name}</Text>
+    <Text style={styles.misc} numberOfLines={1}>{misc}</Text>
   </View>
 );
 
