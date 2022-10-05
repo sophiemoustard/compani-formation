c39c25ac959d1fabc0246be1f1b74e192d5ca960
ulysse <ulysse@alenvi.io>
2022-10-05 17:03:56 +0200

    COM-3201: prerequisite

--- src/components/form/UploadButton/index.tsx
+++ src/components/form/UploadButton/index.tsx
@@ -7,12 +7,13 @@ import styles from './styles';
 
 interface UploadButtonProps {
   title: string,
+  onPress: () => void,
   style?: Object,
   loading?: boolean,
 }
 
-const UploadButton = ({ title, style = {}, loading = false } : UploadButtonProps) => (
-  <TouchableOpacity style={[styles.button, style]}>
+const UploadButton = ({ title, onPress, style = {}, loading = false } : UploadButtonProps) => (
+  <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
     <Text style={styles.title}>{title}</Text>
     {loading && <ActivityIndicator style={commonStyle.disabled} color={GREY[800]} size="small" />}
     {!loading && <Feather name='plus-circle' size={ICON.SM} color={GREY[800]} />}
