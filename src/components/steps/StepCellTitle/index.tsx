e9342fe639cbf78455274f60d971232976147e33
ulysse <ulysse@alenvi.io>
2022-10-13 15:24:31 +0200

    COM-3226: mode is required

--- src/components/steps/StepCellTitle/index.tsx
+++ src/components/steps/StepCellTitle/index.tsx
@@ -9,19 +9,19 @@ type StepCellTitleProps = {
   name: StepType['name'],
   type: StepType['type'],
   index: number,
-  mode?: CourseModeType | null,
+  mode: CourseModeType,
   misc?: string,
   theoreticalHours?: number,
 }
 
-const StepCellTitle = ({ name, type, index, mode = null, misc = '', theoreticalHours = 0 }: StepCellTitleProps) => (
+const StepCellTitle = ({ name, type, index, mode, misc = '', theoreticalHours = 0 }: StepCellTitleProps) => (
   <View style={styles.textContainer}>
     <Text style={styles.stepType}>
       {`ÉTAPE ${index + 1} - ${stepTypeOptions[type]}`}
       {type === E_LEARNING && !!theoreticalHours && ` (${formatDuration(theoreticalHours)})`}
     </Text>
     <Text lineBreakMode={'tail'} numberOfLines={2} style={styles.stepName}>{name}</Text>
-    {mode === TRAINER && <Text style={styles.misc} numberOfLines={1}>{misc}</Text>}
+    {mode === TRAINER && type !== E_LEARNING && <Text style={styles.misc} numberOfLines={1}>{misc}</Text>}
   </View>
 );
 
