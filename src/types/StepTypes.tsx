3eae0e50b81660b7945f0ca2b5d41edb719248d0
Chloe Molle <chloe.molle@neuf.fr>
2022-10-27 18:07:04 +0200

    COM-3249 more types

--- src/types/StepTypes.tsx
+++ src/types/StepTypes.tsx
@@ -6,6 +6,7 @@ type BaseStepType = {
   progress: { live:number, eLearning: number },
   type: string,
   name: string,
+  theoreticalDuration?: string
 }
 
 export type ELearningStepType = BaseStepType &
