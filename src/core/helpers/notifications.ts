6b51ce6a82c8baac29d505f31645b3e1bb4d6bfc
ulysse <ulysse@alenvi.io>
2023-08-01 18:14:08 +0200

    COM-3533: replace deprecated package

--- src/core/helpers/notifications.ts
+++ src/core/helpers/notifications.ts
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
-import Constants from 'expo-constants';
+import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import {
   BLENDED_COURSE_REGISTRATION,
@@ -21,7 +21,7 @@ type ExpoTokenAndStatusType = {
 }
 
 export const registerForPushNotificationsAsync = async (): Promise<ExpoTokenAndStatusType> => {
-  if (!Constants.isDevice) return { token: null, status: DENIED };
+  if (!Device.isDevice) return { token: null, status: DENIED };
 
   const { status: existingStatus } = await Notifications.getPermissionsAsync();
   let finalStatus = existingStatus;
