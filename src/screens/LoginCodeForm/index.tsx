30e88655a19b541db74b2afff7e07278b36cd188
Manon Palin <manonpalin@mbp-de-manon.home>
2023-09-13 13:46:43 +0200

    COM-3543 - add Code in questionnaire

--- src/screens/LoginCodeForm/index.tsx
+++ src/screens/LoginCodeForm/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
-import { View } from 'react-native';
+import { useState, createRef } from 'react';
+import { View, Text, TextInput, TextInputKeyPressEventData } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../types/NavigationType';
 import FeatherButton from '../../components/icons/FeatherButton';
 import ExitModal from '../../components/ExitModal';
+import NiInput from '../../components/form/Input';
 import { GREY } from '../../styles/colors';
 import { ICON } from '../../styles/metrics';
 import styles from './styles';
@@ -13,10 +14,38 @@ interface LoginCodeFormProps extends StackScreenProps<RootStackParamList> {}
 
 const LoginCodeForm = ({ navigation }: LoginCodeFormProps) => {
   const [exitConfirmationModal, setExitConfirmationModal] = useState<boolean>(false);
+  const [code, setCode] = useState<string[]>(['', '', '', '']);
+  const inputRefs: any[] = [
+    createRef(),
+    createRef(),
+    createRef(),
+    createRef(),
+  ];
+  const [lastname, setLastname] = useState<string>('');
+  const [firstname, setFirstname] = useState<string>('');
 
   const goBack = () => {
     setExitConfirmationModal(false);
     navigation.goBack();
+  };
+
+  const onChangeText = (char: string, index: number) => {
+    setCode(code.map((c, i) => (i === index ? char : c)));
+    if (!!char && index + 1 < 4) inputRefs[index + 1].focus();
+  };
+
+  const goPreviousAfterEdit = (index: number) => {
+    inputRefs[index].focus();
+    if (code[index] !== '') onChangeText('', index);
+  };
+
+  const checkKeyValue = (key: TextInputKeyPressEventData['key'], idx: number) => {
+    if (key === 'Backspace') {
+      if (!idx && code[idx] === '') return;
+
+      if (code[idx] === '') goPreviousAfterEdit(idx - 1);
+      else onChangeText('', idx);
+    }
   };
 
   return (
@@ -28,6 +57,21 @@ const LoginCodeForm = ({ navigation }: LoginCodeFormProps) => {
           onPressCancelButton={() => setExitConfirmationModal(false)}
           title="Êtes-vous sûr(e) de cela ?" contentText={'Vous reviendrez à la page d\'accueil.'} />
       </View>
+      <View style={styles.codeContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Code de connexion donné par le formateur</Text>
+          <Text style={styles.required}>*</Text>
+        </View>
+        <View style={styles.code}>
+          {inputRefs.map((k, idx) => (
+            <TextInput ref={(r) => { inputRefs[idx] = r; }} key={`${k}${idx}`} value={code[idx]}
+              onChangeText={char => onChangeText(char, idx)} style={styles.number} placeholder={'_'}
+              onKeyPress={({ nativeEvent }) => checkKeyValue(nativeEvent.key, idx)}
+              maxLength={1} keyboardType={'number-pad'} autoFocus={idx === 0} />))}
+        </View>
+      </View>
+      <NiInput caption={'Nom'} value={lastname} onChangeText={setLastname} type={'text'} required />
+      <NiInput caption={'Prénom'} value={firstname} onChangeText={setFirstname} type={'text'} required />
     </SafeAreaView>
   );
 };
--- src/screens/LoginCodeForm/styles.ts
+++ src/screens/LoginCodeForm/styles.ts
@@ -1,14 +1,46 @@
 import { StyleSheet } from 'react-native';
-import { GREY } from '../../styles/colors';
-import { MARGIN } from '../../styles/metrics';
+import { GREY, PINK, WHITE } from '../../styles/colors';
+import { BORDER_RADIUS, BORDER_WIDTH, MARGIN, PADDING } from '../../styles/metrics';
+import { FIRA_SANS_REGULAR } from '../../styles/fonts';
 
 export default StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: GREY[100],
+    marginHorizontal: MARGIN.LG,
   },
   goBack: {
-    marginTop: MARGIN.MD,
-    marginLeft: MARGIN.MD,
+    marginVertical: MARGIN.MD,
+  },
+  codeContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingVertical: PADDING.MD,
+  },
+  code: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: MARGIN.MD,
+    backgroundColor: WHITE,
+    borderRadius: BORDER_RADIUS.MD,
+    borderWidth: BORDER_WIDTH,
+    borderColor: GREY[600],
+  },
+  number: {
+    ...FIRA_SANS_REGULAR.XXL,
+    marginHorizontal: MARGIN.XXS,
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  title: {
+    ...FIRA_SANS_REGULAR.SM,
+    marginBottom: MARGIN.XS,
+    color: GREY[600],
+  },
+  required: {
+    ...FIRA_SANS_REGULAR.SM,
+    color: PINK[500],
+    paddingHorizontal: PADDING.SM,
   },
 });
