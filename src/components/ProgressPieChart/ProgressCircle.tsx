accecdf692af5491b3694a9b5764015fb2269bcb
ulysse <ulysse@alenvi.io>
2024-03-26 12:04:15 +0100

    COM-3679: remove useless component in progress circle

--- src/components/ProgressPieChart/ProgressCircle.tsx
+++ src/components/ProgressPieChart/ProgressCircle.tsx
@@ -1,6 +1,4 @@
-import { View } from 'react-native';
 import Svg, { Circle } from 'react-native-svg';
-
 import { YELLOW } from '../../styles/colors';
 
 interface ProgressCircleProps {
@@ -15,14 +13,14 @@ const ProgressCircle = ({ progress, strokeWidth, size }: ProgressCircleProps) =>
   const svgProgress = 1 - progress;
 
   return (
-    <View>
+    <>
       <Svg width={size} height={size}>
         <Circle stroke={'transparent'} fill="none" cx={size / 2} cy={size / 2} r={radius} {...{ strokeWidth }} />
         <Circle stroke={YELLOW[500]} fill="none" cx={size / 2} cy={size / 2} r={radius}
           strokeDasharray={`${circum} ${circum}`} strokeDashoffset={radius * Math.PI * 2 * (svgProgress)}
           strokeLinecap="round" transform={`rotate(-90, ${size / 2}, ${size / 2})`} {...{ strokeWidth }} />
       </Svg>
-    </View>
+    </>
   );
 };
 
