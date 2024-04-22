0244d28616b042cb6cb129a2591fcbd0ebc3400e
ulysse <ulysse@alenvi.io>
2024-04-22 19:42:56 +0200

    COM-3684: test

--- src/app/Home/profile/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from 'expo-router';
-
-const ProfilePageLayout = () => (
-  <Stack screenOptions={{ headerShown: false }}>
-    <Stack.Screen name='index'/>
-  </Stack>
-);
-export default ProfilePageLayout;
