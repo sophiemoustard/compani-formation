e92a4d161074dd619a1d5abbcf6a9b9d61332a53
ulysse <ulysse@alenvi.io>
2024-05-15 11:22:47 +0200

    COM-3540: plugins syntax in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = function (api) {
   const platform = api.caller(caller => caller && caller.platform);
+  const plugins = platform !== 'web' ? ['inline-dotenv'] : [];
   api.cache(true);
-  return { presets: ['babel-preset-expo'], plugins: [platform !== 'web' && 'inline-dotenv'].filter(Boolean) };
+  return { presets: ['babel-preset-expo'], plugins };
 };
