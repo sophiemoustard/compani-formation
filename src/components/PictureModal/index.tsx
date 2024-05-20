a7e5aa514ff2afebf277e0ebc5e23fb5f9aceed5
ulysse <ulysse@alenvi.io>
2024-05-20 15:36:34 +0200

    COM-3709: update sdk + code

--- src/components/PictureModal/index.tsx
+++ src/components/PictureModal/index.tsx
@@ -27,6 +27,7 @@ const PictureModal = ({
   deletePicture,
 }: PictureModalProps) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [permission] = Camera.useCameraPermissions();
 
   const alert = (component: string) => {
     Alert.alert(
@@ -40,8 +41,7 @@ const PictureModal = ({
   const requestPermissionsForCamera = async () => {
     try {
       setIsLoading(true);
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      if (status === 'granted') openCamera();
+      if (permission && permission.granted) openCamera();
       else alert('l\'appareil photo');
     } finally {
       setIsLoading(false);
