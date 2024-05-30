ac0741a5f394319678508f7a1288fa796eb2a35c
ulysse <ulysse@alenvi.io>
2024-05-30 11:44:21 +0200

    COM-3712: fix bottom modal

--- src/components/BottomModal/index.tsx
+++ src/components/BottomModal/index.tsx
@@ -1,4 +1,4 @@
-import { View, Modal, ScrollView } from 'react-native';
+import { View, Modal } from 'react-native';
 import FeatherButton from '../icons/FeatherButton';
 import { ICON } from '../../styles/metrics';
 import { GREY } from '../../styles/colors';
@@ -12,13 +12,13 @@ interface NiBottomModalProps {
 
 const BottomModal = ({ visible, children, onRequestClose }: NiBottomModalProps) => (
   <Modal visible={visible} transparent onRequestClose={onRequestClose}>
-    <ScrollView contentContainerStyle={styles.modalContainer}>
+    <View style={styles.modalContainer}>
       <View style={styles.modalContent}>
         <FeatherButton name='x-circle' onPress={onRequestClose} size={ICON.LG} color={GREY[600]}
           style={styles.goBack} />
         {children}
       </View>
-    </ScrollView>
+    </View>
   </Modal>);
 
 export default BottomModal;
