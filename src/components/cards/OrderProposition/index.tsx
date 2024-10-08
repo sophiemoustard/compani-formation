c69c4877e81530d57795027b19f1eda6b93809f9
ulysse <ulysse@alenvi.io>
2024-10-08 13:04:30 +0200

    COM-3792: simplify array

--- src/components/cards/OrderProposition/index.tsx
+++ src/components/cards/OrderProposition/index.tsx
@@ -56,17 +56,11 @@ const OrderProposition = React.forwardRef<OrderPropositionRef, OrderPropositionP
   const [sumOtherHeights, setSumOtherHeights] = useState<number>(
     propsHeight.filter((_, i) => i !== index).reduce((a, b) => a + b, 0)
   );
-  const [allowedPositions, setAllowedPositions] = useState<number[][][]>(
-    [
-      [[0], [propsHeight[1], propsHeight[2]], [sumOtherHeights]],
-      [[-propsHeight[0]], [propsHeight[2] - propsHeight[0], 0], [propsHeight[2]]],
-      [[-sumOtherHeights], [-propsHeight[1], -propsHeight[0]], [0]],
-    ]
-  );
+  const [allowedPositions, setAllowedPositions] = useState<number[][]>([]);
 
   useEffect(() => {
     if (dragCount) {
-      const expectedOffsetY = allowedPositions[index][items[index].tempPosition];
+      const expectedOffsetY = allowedPositions[item.tempPosition];
       if (!expectedOffsetY.includes(lastOffsetY.value)) {
         if (expectedOffsetY.length === 1) {
           lastOffsetY.value = expectedOffsetY[0];
@@ -91,7 +85,7 @@ const OrderProposition = React.forwardRef<OrderPropositionRef, OrderPropositionP
         }
       }
     }
-  }, [allowedPositions, dragCount, index, items, lastOffsetY, propsHeight, sumOtherHeights, translate]);
+  }, [allowedPositions, dragCount, index, item, items, lastOffsetY, propsHeight, sumOtherHeights, translate]);
 
   useEffect(() => {
     if (isGoodPosition && isValidated) {
@@ -114,13 +108,12 @@ const OrderProposition = React.forwardRef<OrderPropositionRef, OrderPropositionP
   useEffect(() => {
     const tempSumOtherHeights = propsHeight.filter((_, i) => i !== index).reduce((a, b) => a + b, 0);
     setSumOtherHeights(tempSumOtherHeights);
-    setAllowedPositions(
-      [
-        [[0], [propsHeight[1], propsHeight[2]], [tempSumOtherHeights]],
-        [[-propsHeight[0]], [propsHeight[2] - propsHeight[0], 0], [propsHeight[2]]],
-        [[-tempSumOtherHeights], [-propsHeight[1], -propsHeight[0]], [0]],
-      ]
-    );
+    const tempAllowedPositions = [
+      [[0], [propsHeight[1], propsHeight[2]], [tempSumOtherHeights]],
+      [[-propsHeight[0]], [propsHeight[2] - propsHeight[0], 0], [propsHeight[2]]],
+      [[-tempSumOtherHeights], [-propsHeight[1], -propsHeight[0]], [0]],
+    ];
+    setAllowedPositions(tempAllowedPositions[index]);
   }, [index, propsHeight]);
 
   const handleLayout = (event: LayoutChangeEvent) => {
