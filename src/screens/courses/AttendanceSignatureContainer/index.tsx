7f13a346d08c7413f031b9dbb1ccac729638f230
ulysse <ulysse@alenvi.io>
2024-10-15 19:07:31 +0200

    COM-3819: sign slot (2)

--- src/screens/courses/AttendanceSignatureContainer/index.tsx
+++ src/screens/courses/AttendanceSignatureContainer/index.tsx
@@ -1,12 +1,12 @@
-import { useCallback, useEffect, useReducer, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef, useState } from 'react';
 import { BackHandler, View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import Signature, { SignatureViewRef } from 'react-native-signature-canvas';
 import { Picker } from '@react-native-picker/picker';
 import { StackScreenProps } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootStackParamList } from '../../../types/NavigationType';
 import CompaniDate from '../../../core/helpers/dates/companiDates';
-import { DD_MM_YYYY, HH_MM, IS_WEB } from '../../../core/data/constants';
+import { DD_MM_YYYY, HH_MM } from '../../../core/data/constants';
 import { ICON } from '../../../styles/metrics';
 import { GREY } from '../../../styles/colors';
 import { ascendingSort } from '../../../core/helpers/dates/utils';
@@ -28,94 +28,9 @@ const AttendanceSignatureContainer = ({ route, navigation }: AttendanceSignature
   const [slotsOptions, setSlotsOptions] = useState<{label: string, value: string}[]>([]);
   const [signature, setSignature] = useState<{slot: string, data: string}>({ slot: '', data: '' });
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const iframeRef = useRef<any>(null);
-  const webViewRef = useRef<WebView>(null);
+  const signatureRef = useRef<SignatureViewRef>(null);
   const userId = useGetLoggedUserId();
   const [error, dispatchError] = useReducer(errorReducer, initialErrorState);
-
-  const htmlContent = `
-    <html>
-      <head>
-        <style>
-          .wrapper {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            -moz-user-select: none;
-            -webkit-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-          }
-          .signature-pad {
-            position: absolute;
-            left: 0;
-            top: 0;
-            width:100%;
-            height:100%;
-            background-color: white;
-          }
-        </style>
-        <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.0.0/dist/signature_pad.umd.min.js"></script>
-      </head>
-      <body>
-        <div class="wrapper">
-          <canvas id="signature-pad" class="signature-pad"></canvas>
-        </div>
-        <script>
-          let canvas = document.getElementById('signature-pad');
-            
-          const resizeCanvas = () => {
-            let ratio = Math.max(window.devicePixelRatio || 1, 1);
-            canvas.width = canvas.offsetWidth * ratio;
-            canvas.height = canvas.offsetHeight * ratio;
-            canvas.getContext("2d").scale(ratio, ratio);
-          }
-          window.onresize = resizeCanvas;
-          resizeCanvas();
-              
-          let signaturePad = new SignaturePad(canvas);
-
-          const clearSignature = () => {
-            signaturePad.clear();
-            if (${IS_WEB}) window.parent.postMessage('', "*");
-            else window.ReactNativeWebView.postMessage('');
-          }
-
-          const undoSignature = () => {
-            let data = signaturePad.toData();
-            if (data) {
-              data.pop();
-              signaturePad.fromData(data);
-              if (signaturePad.isEmpty()) {
-                signaturePad.clear();
-                if (${IS_WEB}) window.parent.postMessage('', "*");
-                else window.ReactNativeWebView.postMessage('');
-              } else {
-                let dataUrl = signaturePad.toDataURL('image/png');
-                if (${IS_WEB}) window.parent.postMessage(dataUrl, "*");
-                else window.ReactNativeWebView.postMessage(dataUrl);
-              }
-            }
-          }
-
-          const handleMessage = (message) => {
-            if (message === 'clear') {
-              clearSignature();
-            } else if (message === 'undo') {
-              undoSignature();
-            }
-          }
-
-          signaturePad.addEventListener('endStroke', () => {
-            let dataUrl = signaturePad.toDataURL('image/png');
-            if (${IS_WEB}) window.parent.postMessage(dataUrl, "*");
-            else window.ReactNativeWebView.postMessage(dataUrl);
-          });
-          window.addEventListener('message', (event) => handleMessage(event.data));
-        </script>
-      </body>
-    </html>
-  `;
 
   useEffect(() => {
     setSlotsOptions(
@@ -128,40 +43,9 @@ const AttendanceSignatureContainer = ({ route, navigation }: AttendanceSignature
     );
   }, [slots]);
 
-  const onMessage = (event: any) => {
-    const dataURI = event.nativeEvent.data;
-    setSignature(prevSignature => ({ ...prevSignature, data: dataURI }));
-    if (dataURI) {
-      dispatchError({ type: RESET_ERROR });
-    }
-  };
-
-  const handleIframeMessage = (event: any) => {
-    // eslint-disable-next-line no-undef
-    if (event.origin !== window.location.origin && event.origin !== 'null') return;
-    const dataURI = event.data;
-    setSignature(prevSignature => ({ ...prevSignature, data: dataURI }));
-    if (dataURI) {
-      dispatchError({ type: RESET_ERROR });
-    }
-  };
-
-  useEffect(() => {
-    if (IS_WEB) {
-      // eslint-disable-next-line no-undef
-      window.addEventListener('message', handleIframeMessage);
-    }
-    return () => {
-      if (IS_WEB) {
-        // eslint-disable-next-line no-undef
-        window.removeEventListener('message', handleIframeMessage);
-      }
-    };
-  }, []);
-
-  const goBack = useCallback(() => {
+  const goBack = async () => {
     navigation.navigate('LearnerCourseProfile', { courseId: profileId });
-  }, [navigation, profileId]);
+  };
 
   const hardwareBackPress = () => {
     goBack();
@@ -175,41 +59,11 @@ const AttendanceSignatureContainer = ({ route, navigation }: AttendanceSignature
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const sendMessageToWebView = (message: string) => {
-    webViewRef.current?.injectJavaScript(`handleMessage("${message}"); true;`);
-  };
-
-  const sendMessageToIframe = (message: string) => {
-    iframeRef.current?.contentWindow.postMessage(message, '*');
-  };
-
-  const base64ToBlob = (base64Data: string, contentType: string) => {
-    const byteCharacters = atob(base64Data.split(',')[1]);
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-      const slice = byteCharacters.slice(offset, offset + 512);
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i += 1) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
-
-    return new Blob(byteArrays, { type: contentType });
-  };
-
   const savePicture = async () => {
     try {
       if (signature.slot && signature.data) {
         setIsLoading(true);
-        let file;
-        const contentType = 'image/png';
-        if (IS_WEB) {
-          const blob = base64ToBlob(signature.data, contentType);
-          file = new File([blob], `signature-${userId}.png`, { type: contentType });
-        } else file = { uri: signature.data, type: contentType, name: `signature-${userId}` };
+        const file = { uri: signature.data, type: 'image/png', name: `signature-${userId}` };
         const data = formatPayload({
           signature: file,
           course: profileId,
@@ -219,8 +73,7 @@ const AttendanceSignatureContainer = ({ route, navigation }: AttendanceSignature
         await attendanceSheets.upload(data);
         setIsLoading(false);
         setSignature({ slot: '', data: '' });
-        if (IS_WEB) sendMessageToIframe('clear');
-        else sendMessageToWebView('clear');
+        signatureRef.current!.clearSignature();
         goBack();
         dispatchError({ type: RESET_ERROR });
       } else {
@@ -231,9 +84,24 @@ const AttendanceSignatureContainer = ({ route, navigation }: AttendanceSignature
     }
   };
 
-  const clearCanvas = () => (IS_WEB ? sendMessageToIframe('clear') : sendMessageToWebView('clear'));
+  const save = (uri: any) => {
+    setSignature(prevSignature => ({ ...prevSignature, data: uri }));
+    dispatchError({ type: RESET_ERROR });
+  };
 
-  const undoCanvas = () => (IS_WEB ? sendMessageToIframe('undo') : sendMessageToWebView('undo'));
+  const remove = () => {
+    setSignature(prevSignature => ({ ...prevSignature, data: '' }));
+  };
+
+  const onEnd = () => {
+    signatureRef.current!.readSignature();
+  };
+
+  const style = `.m-signature-pad {box-shadow: none; border: none; } 
+                .m-signature-pad--body {border: none;}
+                .m-signature-pad--footer {display: none; margin: 0px;}
+                body,html {
+                width: 100%; height: 100%;}`;
 
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
@@ -244,25 +112,12 @@ const AttendanceSignatureContainer = ({ route, navigation }: AttendanceSignature
         {slotsOptions.map(slot => (<Picker.Item key={slot.value} label={slot.label} value={slot.value} />))}
       </Picker>
       <View style={styles.webviewContainer}>
-        {
-          IS_WEB
-            ? <iframe
-              ref={iframeRef}
-              src={`data:text/html,${encodeURIComponent(htmlContent)}`}
-              style={{ width: '100%', height: 400, border: '1px solid #ccc' }}
-            />
-            : <WebView
-              ref={webViewRef}
-              source={{ html: htmlContent, baseUrl: '' }}
-              onMessage={onMessage}
-              javaScriptEnabled
-              originWhitelist={['*']}
-            />
-        }
+        <Signature webStyle={style} ref={signatureRef} onOK={save} onEnd={onEnd} onClear={remove} onUndo={onEnd}
+          onEmpty={remove}/>
       </View>
       <View style={styles.buttonContainer}>
-        <NiSecondaryButton caption="Tout effacer" onPress={clearCanvas} />
-        <NiSecondaryButton caption="Annuler" onPress={undoCanvas} />
+        <NiSecondaryButton caption="Tout effacer" onPress={() => { signatureRef.current!.clearSignature(); }} />
+        <NiSecondaryButton caption="Annuler" onPress={() => { signatureRef.current!.undo(); }} />
       </View>
       {!!signature.slot && <NiPrimaryButton caption='Enregistrer la signature' onPress={savePicture}
         loading={isLoading} />}
