61e917ac25081f8753b9e665af6ee324eec680d8
Manon Palin <manonpalin@mbp-de-manon.home>
2024-11-19 14:05:23 +0100

    COM-3835 - create TrainerCell

--- src/screens/explore/BlendedAbout/styles.ts
+++ src/screens/explore/BlendedAbout/styles.ts
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
-import { GREY, TRANSPARENT_GREY } from '../../../styles/colors';
-import { BORDER_RADIUS, BORDER_WIDTH, ICON, MARGIN } from '../../../styles/metrics';
-import { FIRA_SANS_REGULAR, FIRA_SANS_BOLD, FIRA_SANS_MEDIUM, NUNITO_SEMI } from '../../../styles/fonts';
+import { GREY } from '../../../styles/colors';
+import { MARGIN } from '../../../styles/metrics';
+import { FIRA_SANS_REGULAR, FIRA_SANS_BOLD, NUNITO_SEMI } from '../../../styles/fonts';
 
 const styles = StyleSheet.create({
   content: {
@@ -16,25 +16,6 @@ const styles = StyleSheet.create({
     ...FIRA_SANS_REGULAR.MD,
     color: GREY[800],
     marginBottom: MARGIN.MD,
-  },
-  subSectionContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: MARGIN.SM,
-  },
-  subSectionTitle: {
-    ...FIRA_SANS_MEDIUM.LG,
-    color: GREY[900],
-    flexWrap: 'wrap',
-    flex: 1,
-  },
-  trainerPicture: {
-    height: ICON.XXL,
-    width: ICON.XXL,
-    marginRight: MARGIN.MD,
-    borderRadius: BORDER_RADIUS.XXL,
-    borderWidth: BORDER_WIDTH,
-    borderColor: TRANSPARENT_GREY,
   },
   internalRulesContainer: {
     justifyContent: 'center',
