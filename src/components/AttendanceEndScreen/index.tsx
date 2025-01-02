7487b5e75b69b74b4d8810ee913cb81c6231b207
ulysse <ulysse@alenvi.io>
2025-01-02 12:07:33 +0100

    COM-3854: simplify code

--- src/components/AttendanceEndScreen/index.tsx
+++ src/components/AttendanceEndScreen/index.tsx
@@ -13,50 +13,50 @@ interface AttendanceEndScreenProps {
   mode?: string
 }
 
-const AttendanceEndScreen = ({ traineeName, failUpload, goToNextScreen, mode = TRAINER }: AttendanceEndScreenProps) => (
-  <SafeAreaView style={styles.safeArea} edges={['top']}>
-    {mode === TRAINER ? <>
-      {failUpload
-        ? <View style={styles.errorContainer}>
-          <Text style={styles.title}>Echec de l&apos;envoi de la demande à {traineeName}</Text>
-          <MaterialIcons style={styles.icon} size={200} name={'warning'} color={PINK[500]} />
-          <Text style={styles.text}>Veuillez réitérer votre demande</Text>
-        </View>
-        : <>
-          <View style={styles.upperContainer}>
-            <Text style={styles.title}>Une demande d&apos;émargement a été envoyée à {traineeName}</Text>
-          </View>
-          <Text style={styles.text}>Elle est disponible sur la page de la formation sur son application mobile</Text>
-          <Image source={require('../../../assets/images/aux_fierte.webp')} style={styles.image} />
-          <Text style={styles.text}>
-            N&apos;oubliez pas de reporter les émargements dans le tableau prévu à cet effet sur l&apos;application
-            web Compani
-          </Text>
-        </>
-      }
-    </>
-      : <>
+const AttendanceEndScreen = ({ traineeName, failUpload, goToNextScreen, mode = TRAINER }: AttendanceEndScreenProps) => {
+  const renderFailMessage = (text: string) => <View style={styles.errorContainer}>
+    <Text style={styles.title}>{text}</Text>
+    <MaterialIcons style={styles.icon} size={200} name={'warning'} color={PINK[500]} />
+    <Text style={styles.text}>Veuillez réitérer votre demande</Text>
+  </View>;
+
+  return (
+    <SafeAreaView style={styles.safeArea} edges={['top']}>
+      {mode === TRAINER ? <>
         {failUpload
-          ? <View style={styles.errorContainer}>
-            <Text style={styles.title}>Echec lors de l&apos;envoi de votre signature</Text>
-            <MaterialIcons style={styles.icon} size={200} name={'warning'} color={PINK[500]} />
-            <Text style={styles.text}>Veuillez réitérer votre demande</Text>
-          </View>
+          ? renderFailMessage(`Echec de l'envoi de la demande à ${traineeName}`)
           : <>
             <View style={styles.upperContainer}>
-              <Text style={styles.title}>Merci d&apos;avoir émargé les créneaux</Text>
+              <Text style={styles.title}>Une demande d&apos;émargement a été envoyée à {traineeName}</Text>
             </View>
+            <Text style={styles.text}>Elle est disponible sur la page de la formation sur son application mobile</Text>
             <Image source={require('../../../assets/images/aux_fierte.webp')} style={styles.image} />
             <Text style={styles.text}>
-              Vous pouvez désormais retourner sur votre formation pour émarger d&apos;autres créneaux ou poursuivre
-              votre apprentissage
+            N&apos;oubliez pas de reporter les émargements dans le tableau prévu à cet effet sur l&apos;application
+            web Compani
             </Text>
           </>
         }
-      </>}
-    <View style={styles.footer}>
-      <NiPrimaryButton caption={'Terminer'} onPress={goToNextScreen} />
-    </View>
-  </SafeAreaView>
-);
+      </>
+        : <>
+          {failUpload
+            ? renderFailMessage('Echec lors de l\'envoi de votre signature')
+            : <>
+              <View style={styles.upperContainer}>
+                <Text style={styles.title}>Merci d&apos;avoir émargé les créneaux</Text>
+              </View>
+              <Image source={require('../../../assets/images/aux_fierte.webp')} style={styles.image} />
+              <Text style={styles.text}>
+              Vous pouvez désormais retourner sur votre formation pour émarger d&apos;autres créneaux ou poursuivre
+              votre apprentissage
+              </Text>
+            </>
+          }
+        </>}
+      <View style={styles.footer}>
+        <NiPrimaryButton caption={'Terminer'} onPress={goToNextScreen} />
+      </View>
+    </SafeAreaView>
+  );
+};
 export default AttendanceEndScreen;
